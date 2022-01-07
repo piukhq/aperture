bff0782df0d7e3220b89ced813bfcb3b9a15be39
Lukie Kang <lkang@bink.com>
2022-01-07 10:15:31 +0000

    add initial font import into correct location

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,14 @@
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    fontFamily: {
+      sans: ['Nunito Sans', 'sans-serif'],
+    },
     extend: {
-      colors: {
-        'off-white-1': '#e2e2ea',
-        'off-white-2': '#f1f1f5',
-        'off-black': '#171725',
-        'off-grey-1': '#44444f',
-        'off-grey-2': '#92929d',
-        'off-grey-3': '#696974',
-        'blue': '#0062ff',
-      },
     },
   },
   plugins: [],
