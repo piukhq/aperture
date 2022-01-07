1b0402072597a4346bdea29f5e51804a1026f031
Lukie Kang <lkang@bink.com>
2022-01-07 16:18:25 +0000

    add initial color palette and typography

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -3,22 +3,22 @@ import Link from 'next/link'
 export default function Layout({children}){
   return (
     <>
+      <nav className='bg-binkGreen'>
+        <ul>
+          <li>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/design">
+              <a>Design</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <hr />
       <main>
-        <nav>
-          <ul>
-            <li>
-              <Link href="/">
-                <a>Home</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/design">
-                <a>Design</a>
-              </Link>
-            </li>
-          </ul>
-        </nav>
-        <hr />
         {children}
       </main>
     </>
