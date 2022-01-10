acfdfe58e0e8819c5b33b4430dd9ae39e968407d
Lukie Kang <lkang@bink.com>
2022-01-10 21:39:10 +0000

    add first draft of common design elements with draft layout as a starting point

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,26 +1,28 @@
 import Link from 'next/link'
+import Image from 'next/Image'
 
 export default function Layout({children}){
   return (
-    <>
-      <nav className='bg-binkGreen'>
-        <ul>
+    <div className='flex flex-none h-full min-h-screen'>
+      <nav className='flex flex-col items-center bg-white w-72 shrink-0 sticky top-0 drop-shadow-lg'>
+        <ul className='flex flex-col items-center shrink-0 sticky top-0'>
           <li>
-            <Link href="/">
-              <a>Home</a>
+            <Link href='/' passHref>
+              <Image src='/images/menu-bink.png' width='280' height='200' alt='Home' />
             </Link>
           </li>
           <li>
-            <Link href="/design">
-              <a>Design</a>
+            <Link href='/design' passHref>
+              <button className='flex justify-start items-center w-60 h-12 bg-primaryGreen/12 text-primaryGreen font-semibold rounded-xl'>
+                <span className='px-3 pt-2'><Image src='/images/menu-assets.png'alt='' height='18' width='23' /></span><span>Design system - dev only</span>
+              </button>
             </Link>
           </li>
         </ul>
       </nav>
-      <hr />
       <main>
         {children}
       </main>
-    </>
-  );
+    </div>
+  )
 }
