f70c3cedb1409f515c60cae8d58b46dd164e29af
Lukie Kang <lkang@bink.com>
2022-01-17 16:05:19 +0000

    Create esLint rules and fix issues with existing code

--- jest.config.js
+++ jest.config.js
@@ -7,12 +7,12 @@ const createJestConfig = nextJest({
 
 // Add any custom config to be passed to Jest
 const customJestConfig = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'], 
-  // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  // If using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
-  testPathIgnorePatterns: ['<rootDir>/cypress/']
+  testPathIgnorePatterns: ['<rootDir>/cypress/'],
 }
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+// CreateJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 module.exports = createJestConfig(customJestConfig)
