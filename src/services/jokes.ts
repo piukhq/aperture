1f335ee91dead6cef77167b72974e83e9a1c1206
George <godonnell@bink.com>
2022-01-20 15:07:03 +0000

    Added linting on file save

--- src/services/jokes.ts
+++ src/services/jokes.ts
@@ -1,4 +1,4 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
 type RandomJokeResponse =
   | {
@@ -14,15 +14,14 @@ type RandomJokeResponse =
 // Define a service using a base URL and expected endpoints
 export const jokeApi = createApi({
   reducerPath: 'jokeApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://v2.jokeapi.dev/' }),
+  baseQuery: fetchBaseQuery({baseUrl: 'https://v2.jokeapi.dev/'}),
   endpoints: builder => ({
     getRandomJoke: builder.query<RandomJokeResponse, void>({
-      query: () =>
-        `joke/Programming?blacklistFlags=explicit,nsfw,political,racist,religious,sexist`,
+      query: () => 'joke/Programming?blacklistFlags=explicit,nsfw,political,racist,religious,sexist',
     }),
   }),
 })
 
 // Export hooks for usage in functional components, which are
 // auto-generated based on the defined endpoints
-export const { useGetRandomJokeQuery } = jokeApi
+export const {useGetRandomJokeQuery} = jokeApi
