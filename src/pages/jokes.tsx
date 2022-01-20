4ea31398bac22a6af8074b11a27fc62c475d30ca
George <godonnell@bink.com>
2022-01-20 13:20:52 +0000

    Added RTK Query example implementation

---
+++ src/pages/jokes.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import { useGetRandomJokeQuery } from '../services/jokes'
+
+
+const Jokes = () => {
+  const [skip, setSkip] = useState(true)
+  const { data, error, isLoading } = useGetRandomJokeQuery(null, {
+    skip, // Used to conditionally call fetch api
+  })
+
+  return (
+    <div>
+      <h2>Generate random joke</h2>
+      {isLoading && <p>Loading...</p>}
+      {data && <p>{JSON.stringify(data)}</p>}
+      {error && <p>Oops, something went wrong</p>}
+      <button onClick={() => setSkip(prev => !prev)}>
+        Generate joke
+      </button>
+    </div>
+  );
+};
+
+export default Jokes;
---
+++ src/services/jokes.ts
@@ -0,0 +1,28 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+type RandomJokeResponse =
+  | {
+      type: 'twopart'
+      setup: string
+      delivery: string
+    }
+  | {
+      type: 'single'
+      joke: string
+    }
+
+// Define a service using a base URL and expected endpoints
+export const jokeApi = createApi({
+  reducerPath: 'jokeApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://v2.jokeapi.dev/' }),
+  endpoints: builder => ({
+    getRandomJoke: builder.query<RandomJokeResponse, void>({
+      query: () =>
+        `joke/Programming?blacklistFlags=explicit,nsfw,political,racist,religious,sexist`,
+    }),
+  }),
+})
+
+// Export hooks for usage in functional components, which are
+// auto-generated based on the defined endpoints
+export const { useGetRandomJokeQuery } = jokeApi
