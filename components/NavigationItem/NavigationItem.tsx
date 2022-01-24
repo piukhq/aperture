bd170cb5197b2d54c6dccad2a32146f1675165f0
Lukie Kang <lkang@bink.com>
2022-01-24 14:12:27 +0000

    create intial navigation item

---
+++ components/NavigationItem/NavigationItem.tsx
@@ -0,0 +1,42 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import dynamic from 'next/dynamic'
+
+interface NavigationItemType {
+  path: string,
+  iconPath: string,
+  label: string,
+}
+const NavigationItem = ({ 
+  path,
+  iconPath,
+  label
+}: NavigationItemType) => {
+
+  // something to check URL matches path to set always blue
+  const { asPath } = useRouter()
+  const isCurrentPage = asPath === path
+
+  const IconSvg = dynamic(() => import(iconPath))
+
+  console.log(iconPath)
+
+  const getClasses = () => {
+    return isCurrentPage ? 'flex items-center my-[20px] w-[200px] h-[32px] bg-white font-heading font-medium text-sm tracking-[0.1px] text-blue dark:text-grey-500 border-l-4 border-l-blue '
+    : 'flex items-center w-[200px] h-[32px] bg-white font-heading font-medium text-sm tracking-[0.1px] text-grey-800 dark:text-grey-500 rounded-xl'
+  }
+
+  return (
+    <div className='bg-white'>
+      <Link href={path} passHref>
+        <button className={getClasses()}>
+        <img src={iconPath} />
+        < IconSvg />{label}
+        </button>
+      </Link>
+    </div>
+      
+  )
+}
+
+export default NavigationItem
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,10 +4,10 @@ const Document = () => (
   <Html>
     <Head>
       <link rel="preconnect" href="https://fonts.googleapis.com"/>
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true'/>
       <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet"/>
     </Head>
-    <body className='bg-white dark:bg-grey-800'>
+    <body className='bg-grey-100 dark:bg-grey-800'>
       <Main />
       <NextScript />
     </body>
