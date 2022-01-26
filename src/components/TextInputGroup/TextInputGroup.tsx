8657ae61d0492c5dc70b3ee5a5c5c96b9e7b45f8
Lukie Kang <lkang@bink.com>
2022-01-26 00:13:01 +0000

    Add additional inputs

--- src/components/TextInputGroup/TextInputGroup.tsx
+++ src/components/TextInputGroup/TextInputGroup.tsx
@@ -1,101 +1,156 @@
 import React, {ReactNode} from 'react'
 import {classNames} from 'utils/classNames'
 
-// Type, Colour, Border, Icon something
-
 enum InputType {
-  NAME,
+  TEXT,
   PASSWORD,
+  SEARCH,
+  SELECT,
 }
-enum Colour {
+
+enum InputWidth {
+  FULL,
+  LARGE,
+  MEDIUM,
+}
+
+enum InputColour {
   BLUE,
   RED,
   GREY,
+  DARK_GREY,
   LIGHT_GREY,
   GREEN,
 }
 
-enum Border {
+enum InputStyle {
   FULL,
   UNDERLINE_ICON_LEFT,
   UNDERLINE_ICON_RIGHT,
   FULL_SMALL,
+  FULL_SMALL_LABEL_HIDDEN,
   UNDERLINE_ICON_LEFT_SMALL,
   UNDERLINE_ICON_RIGHT_SMALL,
+  UNDERLINE_SMALL_LABEL_HIDDEN,
+  WHITE_ICON_LEFT_SMALL,
   WHITE_ICON_LEFT,
+  WHITE_ICON_RIGHT_SMALL,
+  WHITE_ICON_RIGHT,
 }
 
 
-const INPUTTYPE_MAPS: Record<InputType, string> = {
-  [InputType.NAME]: 'name',
+const INPUT_TYPE_MAPS: Record<InputType, string> = {
+  [InputType.TEXT]: 'text',
   [InputType.PASSWORD]: 'password',
+  [InputType.SEARCH]: 'search',
+  [InputType.SELECT]: 'select',
 }
 
-const COLOUR_MAPS: Record<Colour, { label: string, input: string}> = {
-  [Colour.BLUE]: {
+const INPUT_WIDTH_MAPS: Record<InputWidth, string> = {
+  [InputWidth.FULL]: 'w-full',
+  [InputWidth.LARGE]: 'w-[500px]',
+  [InputWidth.MEDIUM]: 'w-[260px]',
+}
+
+const INPUT_COLOUR_MAPS: Record<InputColour, { label: string, input: string}> = {
+  [InputColour.BLUE]: {
     label: 'text-blue',
     input: 'border-lightBlue border-b-lightBlue',
   },
-  [Colour.RED]: {
+  [InputColour.RED]: {
     label: 'text-red',
     input: 'border-red border-b-red',
   },
-  [Colour.GREY]: {
+  [InputColour.GREY]: {
     label: 'text-grey-500',
     input: 'border-grey-500 border-b-grey-500',
   },
-  [Colour.LIGHT_GREY]: {
+  [InputColour.DARK_GREY]: {
+    label: 'text-grey-500',
+    input: 'border-grey-700 border-b-grey-700',
+  },
+  [InputColour.LIGHT_GREY]: {
     label: 'text-grey-300',
     input: 'border-grey-300 border-b-grey-300',
   },
-  [Colour.GREEN]: {
+  [InputColour.GREEN]: {
     label: 'text-green',
     input: 'border-green border-b-green',
   },
 }
 
-const BORDER_MAPS: Record<Border, { container: string, label: string, input: string, icon: string}> = {
-  [Border.UNDERLINE_ICON_LEFT]: {
-    container: 'w-full h-[48px]',
+const INPUT_STYLE_MAPS: Record<InputStyle, { container: string, label: string, input: string, icon: string}> = {
+  [InputStyle.UNDERLINE_ICON_LEFT]: {
+    container: 'h-[48px]',
     label: 'hidden',
-    input: 'w-full h-[48px] pl-6 border-b-[1px] border-b-grey-400 bg-transparent font-body text-base tracking-[0.1px]',
+    input: 'w-full h-[48px] pl-7 border-b-[1px] border-b-grey-400 bg-transparent font-body text-base tracking-[0.1px]',
+    icon: 'absolute top-[13px] left-[1px]',
+  },
+  [InputStyle.UNDERLINE_ICON_LEFT_SMALL]: {
+    container: 'h-[42px]',
+    label: 'hidden',
+    input: 'w-full h-[39px] pl-7 border-b-[1px] border-b-grey-400 bg-transparent font-body text-sm tracking-[0.1px]',
     icon: 'absolute top-[10px] left-[1px]',
   },
-  [Border.UNDERLINE_ICON_LEFT_SMALL]: {
-    container: 'w-full h-[42px]',
-    label: 'hidden',
-    input: 'w-full h-[26px] pl-6 border-b-[1px] border-b-grey-400 bg-transparent font-body text-sm tracking-[0.1px]',
-    icon: 'absolute top-[10px] left-[1px]',
-  },
-  [Border.UNDERLINE_ICON_RIGHT]: {
-    container: 'w-full h-[58px]',
+  [InputStyle.UNDERLINE_ICON_RIGHT]: {
+    container: 'h-[58px]',
     label: 'absolute top-[-13px] px-1 dark:bg-grey-800 text-2xs text-grey-500 font-body bg-grey-100 z-10',
-    input: 'w-full h-[39px] p-1 border-b-[1px] border-b-grey-400 bg-transparent font-body text-base tracking-[0.1px]',
+    input: 'w-full h-[39px] px-1 border-b-[1px] border-b-grey-400 bg-transparent font-body text-base tracking-[0.1px]',
     icon: 'absolute top-[10px] right-[1px]',
   },
-  [Border.UNDERLINE_ICON_RIGHT_SMALL]: {
-    container: 'w-full h-[55px]',
+  [InputStyle.UNDERLINE_ICON_RIGHT_SMALL]: {
+    container: 'h-[55px]',
     label: 'absolute top-[-13px] px-1 dark:bg-grey-800 text-2xs text-grey-500 font-body bg-grey-100 z-10 tracking-[0px]',
-    input: 'w-full h-[16px] p-1 border-b-[1px] border-b-grey-400 bg-transparent font-body text-sm tracking-[0.09px]',
+    input: 'w-full h-[39px] p-1 border-b-[1px] border-b-grey-400 bg-transparent font-body text-sm tracking-[0.09px]',
     icon: 'absolute top-[10px] right-[1px]',
   },
-  [Border.FULL]: {
-    container: 'w-full h-[48px]',
+  [InputStyle.UNDERLINE_SMALL_LABEL_HIDDEN]: {
+    container: 'h-[55px]',
+    label: 'hidden',
+    input: 'w-full h-[39px] p-1 border-b-[1px] border-b-grey-400 bg-transparent font-body text-sm tracking-[0.09px]',
+    icon: 'absolute top-[10px] right-[1px]',
+  },
+  [InputStyle.FULL]: {
+    container: 'h-[48px]',
     label: 'absolute top-[-8px] left-[13px] px-1 dark:bg-grey-800 text-2xs font-body bg-grey-100 z-10',
-    input: 'w-full h-full p-4 bg-transparent border rounded-[10px] font-body text-base tracking-[0.1px] text-grey-800',
+    input: 'w-full h-full p-4 bg-transparent border rounded-[10px] font-body text-sm tracking-[0.1px] text-grey-800',
     icon: null,
   },
-  [Border.FULL_SMALL]: {
-    container: 'w-full h-[42px]',
+  [InputStyle.FULL_SMALL]: {
+    container: 'h-[42px]',
     label: 'absolute top-[-8px] left-[13px] px-1 dark:bg-grey-800 text-3xs font-body bg-grey-100 z-10',
     input: 'w-full h-full p-4 bg-transparent border rounded-[10px] font-body text-sm tracking-[0.1px] text-grey-800',
     icon: null,
   },
-  [Border.WHITE_ICON_LEFT]: {
-    container: 'w-full h-[42px]',
+  [InputStyle.FULL_SMALL_LABEL_HIDDEN]: {
+    container: 'h-[42px]',
     label: 'hidden',
-    input: 'w-full h-full p-8 border border-grey-300 bg-white rounded-[10px] font-body text-sm tracking-[0.1px] text-grey-800',
+    input: 'w-full h-full p-4 bg-transparent border rounded-[10px] font-body text-sm tracking-[0.1px] text-grey-800',
+    icon: null,
+  },
+  [InputStyle.WHITE_ICON_LEFT_SMALL]: {
+    container: 'h-[42px]',
+    label: 'hidden',
+    input: 'w-full h-[39px] pl-8 bg-white rounded-[10px] font-body text-sm tracking-[0.1px]',
     icon: 'absolute top-[10px] left-[1px]',
+  },
+  [InputStyle.WHITE_ICON_RIGHT_SMALL]: {
+    container: 'h-[42px]',
+    label: 'hidden',
+    input: 'w-full h-[39px] pl-7 bg-white rounded-[10px] font-body text-sm tracking-[0.1px]',
+    icon: 'absolute top-[10px] right-[1px]',
+  },
+  [InputStyle.WHITE_ICON_LEFT]: {
+    container: 'h-[50px]',
+    label: 'hidden',
+    input: 'w-full h-[39px] pl-8 bg-white rounded-[10px] font-body text-sm tracking-[0.1px]',
+    icon: 'absolute top-[10px] left-[1px]',
+  },
+  [InputStyle.WHITE_ICON_RIGHT]: {
+    container: 'h-[50px]',
+    label: 'hidden',
+    input: 'w-full h-[39px] pl-7 bg-white font-body text-sm tracking-[0.1px]',
+    icon: 'absolute top-[10px] right-[1px]',
   },
 }
 
@@ -104,61 +159,85 @@ type Props = {
   label: string
   name: string
   error?: string
-  colour: Colour
-  border: Border
+  inputColour: InputColour
+  inputWidth: InputWidth
+  inputStyle: InputStyle
   svgIcon?: ReactNode
   placeholder?: string
   value: string
-  onChange: (event: { target: { value: string}}) => void // Might need tweaking
+  onChange: (event: { target: { value: string}}) => void
 }
-
 export default function TextInputGroup (props: Props) {
-  const {inputType, colour, label, border, svgIcon, error, name, placeholder, value, onChange} = props
+  const {inputType, inputColour, label, inputWidth, inputStyle, svgIcon, error, name, placeholder, value, onChange} = props
 
-  const isOutlineStyle = border === Border.FULL || border === Border.FULL_SMALL
+  const isOutlineStyle = inputStyle === InputStyle.FULL || inputStyle === InputStyle.FULL_SMALL
+
+
+  const renderInputElement = () => (
+    <input
+      type={INPUT_TYPE_MAPS[inputType]}
+      name={name}
+      id={`bink-form-field-${name}`}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+      className={classNames(
+        'w-full h-full font-body text-sm tracking-[0.1px] text-grey-800',
+        INPUT_COLOUR_MAPS[inputColour].input,
+        INPUT_STYLE_MAPS[inputStyle].input,
+      )}
+    />
+  )
+
+  const renderSelectElement = () => (
+    <select
+      name={name}
+      id={`bink-form-field-${name}`}
+      placeholder={placeholder}
+      className={classNames(
+        'w-full h-full font-body text-sm tracking-[0.1px] text-grey-800',
+        INPUT_COLOUR_MAPS[inputColour].input,
+        INPUT_STYLE_MAPS[inputStyle].input,
+      )}
+    >
+      <option>All Emails</option>
+      <option>Recent</option>
+      <option>Drafts</option>
+    </select>
+  )
 
   return (
     <div className={classNames(
       'relative',
-      BORDER_MAPS[border].container,
+      INPUT_STYLE_MAPS[inputStyle].container,
+      INPUT_WIDTH_MAPS[inputWidth],
     )}>
       <label className={classNames(
-        BORDER_MAPS[border].label,
-        isOutlineStyle && COLOUR_MAPS[colour].label,
+        INPUT_STYLE_MAPS[inputStyle].label,
+        isOutlineStyle && INPUT_COLOUR_MAPS[inputColour].label,
       )} >
         {label}
       </label>
-
-      <input
-        type={INPUTTYPE_MAPS[inputType]}
-        name={name}
-        id={`bink-form-field-${name}`}
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-        className={classNames(
-          'w-full h-full font-body text-sm tracking-[0.1px] text-grey-800',
-          COLOUR_MAPS[colour].input,
-          BORDER_MAPS[border].input,
-        )}
-      />
-      {svgIcon && <div className={BORDER_MAPS[border].icon}>
+      {inputType === InputType.SELECT ? renderSelectElement() : renderInputElement()}
+      {svgIcon && <div className={INPUT_STYLE_MAPS[inputStyle].icon}>
         {svgIcon}
       </div>}
-      {error && <span className='w-24 text-body text-sm text-red absolute top-[12px] right-[4px]'>
+      {error && <span className='w-24 text-body text-sm text-right text-red absolute top-1/4 right-[10px]'>
         {error}
       </span>}
     </div>
   )
 }
 TextInputGroup.defaultProps = {
-  inputType: InputType.NAME,
+  inputType: InputType.TEXT,
   label: 'Label',
-  colour: Colour.GREY,
+  inputWidth: InputWidth.MEDIUM,
+  inputColour: InputColour.GREY,
   svgIcon: false,
   error: null,
 }
 
 TextInputGroup.inputType = InputType
-TextInputGroup.colour = Colour
-TextInputGroup.border = Border
+TextInputGroup.inputColour = InputColour
+TextInputGroup.inputWidth = InputWidth
+TextInputGroup.inputStyle = InputStyle
