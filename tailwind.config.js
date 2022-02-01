d1b22d80d92bcae0e6e276a80c44ebfdc62937a2
nathancread93 <45205301+nathancread93@users.noreply.github.com>
2022-02-01 10:22:26 +0000

    revent a previous change

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,6 @@ module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
