7e0334268e23283378c83e73b733ca140a89a9f0
Lukie Kang <lkang@bink.com>
2022-02-02 11:03:43 +0000

    code formatting

--- src/pages/asset-comparator.tsx
+++ src/pages/asset-comparator.tsx
@@ -10,32 +10,35 @@ import SettingsSvg from 'icons/svgs/settings.svg'
 import RefreshSvg from 'icons/svgs/refresh.svg'
 import CheckSvg from 'icons/svgs/check.svg'
 
-const AssetComparatorPage: NextPage = () => { // Uses placeholder logic for determining if the user is validated or not.
+const AssetComparatorPage: NextPage = () => { // TODO: Uses placeholder logic for determining if the user is verified or not. Update when verified and possibly componentise
 
-  const [isValidated, setIsValidated] = useState(false)
+  const [isVerified, setIsVerified] = useState(false)
 
-  const renderValidatedLanding = () => (
-    <div className='grid grid-cols-5 w-full text-center'>
-      <span className='grid place-items-center h-[46px] w-full bg-grey-200'></span>
-      <h2 className='grid place-items-center h-[46px] w-full bg-grey-200 font-table-header text-grey-800'>DEVELOP</h2>
-      <h2 className='grid place-items-center h-[46px] w-full bg-grey-200 font-table-header text-grey-800'>STAGING</h2>
-      <h2 className='grid place-items-center h-[46px] w-full bg-grey-200 font-table-header text-grey-800'>SANDBOX</h2>
-      <h2 className='grid place-items-center h-[46px] w-full bg-grey-200 font-table-header text-grey-800'>PRODUCTION</h2>
-      <p className='col-span-5 mt-[42px] font-subheading-3'>Select a plan above to compare assets</p>
-    </div>
-  )
-
-  const renderNonValidatedLanding = () => (
+  const renderUnverifiedLanding = () => (
     <div className='mt-[50px] flex flex-col items-center gap-4'>
       <h1 className='font-heading-4'>Welcome to the Bink Asset Comparator</h1>
       <p className='font-subheading-3'>Enter credentials above to compare assets across different environments</p>
     </div>
   )
 
+  const renderVerifiedLanding = () => {
+    const tableHeadingClasses = 'grid place-items-center h-[46px] w-full bg-grey-200 font-table-header text-grey-800'
+    return (
+      <div className='grid grid-cols-5 w-full text-center'>
+        <span className='grid place-items-center h-[46px] w-full bg-grey-200'></span>
+        <h2 className={tableHeadingClasses}>DEVELOP</h2>
+        <h2 className={tableHeadingClasses}>STAGING</h2>
+        <h2 className={tableHeadingClasses}>SANDBOX</h2>
+        <h2 className={tableHeadingClasses}>PRODUCTION</h2>
+        <p className='col-span-5 mt-[42px] font-subheading-3'>Select a plan above to compare assets</p>
+      </div>
+    )
+  }
+
   return (
     <PageLayout>
       <div className='flex gap-[20px] h-[60px] justify-end'>
-        { isValidated &&
+        { isVerified &&
           <>
             <PlansList />
             <Button
@@ -59,7 +62,7 @@ const AssetComparatorPage: NextPage = () => { // Uses placeholder logic for dete
           </>
         }
         <Button
-          handleClick={() => setIsValidated(!isValidated)} // Placeholder validation switch
+          handleClick={() => setIsVerified(!isVerified)} // Placeholder validation switch
           buttonSize={Button.buttonSize.MEDIUM_ICON}
           buttonWidth={Button.buttonWidth.AUTO}
           buttonBackground={Button.buttonBackground.BLUE}
@@ -70,7 +73,7 @@ const AssetComparatorPage: NextPage = () => { // Uses placeholder logic for dete
       </div>
 
       <ContentTile>
-        { isValidated ? renderValidatedLanding() : renderNonValidatedLanding()}
+        { isVerified ? renderVerifiedLanding() : renderUnverifiedLanding()}
       </ContentTile>
     </PageLayout>
   )
