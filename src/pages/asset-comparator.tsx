029ebdbc8e194aec63a9e5e7c0a4612f3078a8e9
Lukie Kang <lkang@bink.com>
2022-02-03 13:23:25 +0000

    remove refresh button

--- src/pages/asset-comparator.tsx
+++ src/pages/asset-comparator.tsx
@@ -7,7 +7,6 @@ import PlansList from 'components/PlansList'
 import Button from 'components/Button'
 
 import SettingsSvg from 'icons/svgs/settings.svg'
-import RefreshSvg from 'icons/svgs/refresh.svg'
 import CheckSvg from 'icons/svgs/check.svg'
 
 const AssetComparatorPage: NextPage = () => { // TODO: Uses placeholder logic for determining if the user is verified or not. Update when verified and possibly componentise
@@ -48,15 +47,6 @@ const AssetComparatorPage: NextPage = () => { // TODO: Uses placeholder logic fo
               labelColour={Button.labelColour.WHITE}
               labelWeight={Button.labelWeight.MEDIUM}
             > <CheckSvg/>Load Assets
-            </Button>
-            <Button
-              handleClick={() => console.log('clicked')}
-              buttonSize={Button.buttonSize.MEDIUM_ICON}
-              buttonWidth={Button.buttonWidth.AUTO}
-              buttonBackground={Button.buttonBackground.BLUE}
-              labelColour={Button.labelColour.WHITE}
-              labelWeight={Button.labelWeight.MEDIUM}
-            > <RefreshSvg/>Refresh
             </Button>
           </>
         }
