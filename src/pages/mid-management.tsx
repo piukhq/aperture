ac365940cdd87d531c2432cfceb4b4e796a01d41
Lukie Kang <lkang@bink.com>
2022-02-03 00:27:15 +0000

    center under construction within content tile

--- src/pages/mid-management.tsx
+++ src/pages/mid-management.tsx
@@ -7,8 +7,10 @@ const MidManagementPage: NextPage = () => {
   return (
     <PageLayout>
       <ContentTile>
-        <Image src='/icons/svgs/construction.svg' height={200} width={200} alt='' />
-        <h1 className='font-heading-1'>Page under construction!</h1>
+        <div className='w-full h-full flex flex-col place-content-center items-center'>
+          <Image src='/icons/svgs/construction.svg' height={200} width={200} alt='' />
+          <h1 className='font-heading-1'>Page under construction!</h1>
+        </div>
       </ContentTile>
     </PageLayout>
   )
