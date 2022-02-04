79dd02c7f0ffc8438540577be04c588e8d53d795
Lukie Kang <lkang@bink.com>
2022-02-04 00:51:22 +0000

    remove console.log

--- src/features/modal/reducer.ts
+++ src/features/modal/reducer.ts
@@ -12,7 +12,6 @@ const initialState:ModalState = {modalRequested: 'NO_MODAL'}
 export const modalReducer = createReducer(initialState, builder => {
   builder
     .addCase(requestModal, (state, action) => {
-      console.log(action)
       state.modalRequested = action.payload
     })
 })
