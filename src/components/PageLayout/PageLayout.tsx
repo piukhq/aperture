083224e1ff586fdb364f7e3778caa7f7e52bf9c2
Lukie Kang <lkang@bink.com>
2022-02-18 15:59:29 +0000

    add min width to page layout to prevent looking broken at tiny layouts

--- src/components/PageLayout/PageLayout.tsx
+++ src/components/PageLayout/PageLayout.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const PageLayout = ({children}: Props) => {
   return (
-    <main className='flex w-full flex-col mt-[57px] mr-[63px] mb-[133px] ml-[63px]'>
+    <main className='flex w-full flex-col min-w-[200px] mt-[57px] mr-[63px] mb-[133px] ml-[63px]'>
       {children}
     </main>
   )
