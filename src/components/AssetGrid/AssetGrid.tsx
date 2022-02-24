4e4d69af4bf721180321b2e48a2e3a709c8db844
Lukie Kang <lkang@bink.com>
2022-02-24 08:26:27 +0000

    additional refactors

--- src/components/AssetGrid/AssetGrid.tsx
+++ src/components/AssetGrid/AssetGrid.tsx
@@ -14,13 +14,15 @@ type Props = {
 
 const AssetGrid = ({planAssets}: Props) => {
 
+  const assetTypeNames = ['HERO', 'BANNER', 'OFFERS', 'ICON', 'ASSET', 'REFERENCE', 'PERSONAL OFFERS', 'PROMOTIONS', 'TIER', 'ALT HERO']
   const {dev, staging} = planAssets
-  const typeNames = ['HERO', 'BANNER', 'OFFERS', 'ICON', 'ASSET', 'REFERENCE', 'PERSONAL OFFERS', 'PROMOTIONS', 'TIER', 'ALT HERO']
-
-
+  console.log(planAssets)
   const assetList = []
 
-  typeNames.forEach((typeName, index) => {
+  // When staging is missing it returns undefined in planAssets we need to check/cope for that in the below....and vice useVerifyStagingCredentialsMutation... or check before it rtuns
+
+
+  assetTypeNames.forEach((typeName, index) => {
     const devAssetsOfType = dev.filter(asset => asset.type === index)
     const stagingAssetsOfType = staging.filter(asset => asset.type === index)
     const maxNumberOfAssets = devAssetsOfType.length > stagingAssetsOfType.length ? devAssetsOfType.length : stagingAssetsOfType.length
@@ -33,57 +35,52 @@ const AssetGrid = ({planAssets}: Props) => {
     })
   })
 
-
   const renderLabelColumn = () => assetList.map(assetType => {
-    const labelPerType = []
-    for (let i = 1; i <= assetType.maxNumberOfAssets; i++) {
-      labelPerType.push(
-        <div key={assetType.heading + i} className= 'w-full h-[100px] grid items-center font-table-header'>
-          {assetType.heading} { assetType.maxNumberOfAssets > 1 && i}
-        </div>)
-    }
-    return labelPerType
+    const {heading, maxNumberOfAssets} = assetType
+    return Array.from(Array(maxNumberOfAssets)).map((_, i) => (
+      <div key={heading + i} className= 'w-full h-[100px] grid items-center font-table-header'>
+        {heading} { maxNumberOfAssets > 1 && i + 1}
+      </div>)
+    )
   })
 
 
-  const renderAssetColumn = (env: string) => assetList.map(assetType => {
-    const assetByType = []
-    for (let i = 0; i < assetType.maxNumberOfAssets; i++) {
-      if (assetType[env][i]) {
-        assetByType.push(
-          <div key={assetType[env][i].url} className='relative w-full h-[100px] grid items-center'>
-            <Image
-              alt={assetType[env][i].description}
-              width='150'
-              height='73'
-              objectFit='contain'
-              src={assetType[env][i].url}
-              quality='25'
-              placeholder='blur'
-              blurDataURL='/icons/svgs/dots.svg'
-            />
-          </div>
-        )
-      } else {
-        assetByType.push(
-          <div className='relative w-full h-[100px] grid items-center justify-center'>
-            <CloseSquareSVG/>
-          </div>
-        )
-      }
+  const renderAssetColumn = (env: string) => assetList.map(assetType => Array.from(Array(assetType.maxNumberOfAssets)).map((_, i) => {
+    if (assetType[env][i]) {
+      return (
+        <div key={assetType[env][i].url} className='relative w-full h-[100px] grid items-center'>
+          <Image
+            alt={assetType[env][i].description}
+            width='150'
+            height='73'
+            objectFit='contain'
+            src={assetType[env][i].url}
+            quality='25'
+            placeholder='blur'
+            blurDataURL='/icons/svgs/dots.svg'
+          />
+        </div>
+      )
+    } else {
+      return (
+        <div key={assetType.heading + 1} className='relative w-full h-[100px] grid items-center justify-center'>
+          <CloseSquareSVG/>
+        </div>
+      )
     }
-    return assetByType
-  })
+  }))
 
-  // 1. Need to error proof this.
-  // 2. tidying up different calls below.
-  // 3. get IntersectionObserver4. test wierd states
+
+  // 1. Need to error proof this when env is not availible...
+  // 2. get meshed in with George's work in Select eleemnt
+  // 3. Test with all plans
+
 
   return (
     <div className='grid grid-cols-5 gap-2 grid-flow-col w-full text-center'>
       <div>{renderLabelColumn()}</div>
-      <div>{renderAssetColumn('dev')}</div>
-      <div>{renderAssetColumn('staging')}</div>
+      <div>{dev && renderAssetColumn('dev')}</div>
+      <div>{staging && renderAssetColumn('staging')}</div>
     </div>
   )
 }
