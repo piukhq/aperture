237cac0c4b0a8714e08b4044c3519dda9decfe71
Lukie Kang <lkang@bink.com>
2022-02-25 00:54:18 +0000

    add assetError handling

--- src/components/AssetGrid/components/Asset.tsx
+++ src/components/AssetGrid/components/Asset.tsx
@@ -4,21 +4,28 @@ import DotsSVG from 'icons/svgs/dots.svg'
 
 export const Asset = ({description, url}) => {
   const [isLoaded, setIsLoaded] = useState(false)
+  const [imageSrc, setImageSrc] = useState(url)
+  const [imageWidth, setImageWidth] = useState('150')
   const imageClasses = isLoaded ? 'opacity-100 transition-opacity' : 'opacity-25 transition-opacity'
 
+  const handleError = () => {
+    setImageSrc('/icons/svgs/asset-error.svg')
+    setImageWidth('22')
+  }
   return (
     <div>
       <Image
         className={imageClasses}
         alt={description}
-        width='150'
+        width={imageWidth}
         height='73'
         objectFit='contain'
-        src={url}
+        src={imageSrc}
         quality='25' // TODO: Revisit this once the hover zoom effect is in place
         placeholder='blur'
         blurDataURL={url}
         onLoadingComplete={() => setIsLoaded(true)}
+        onError={handleError}
       />
 
       {!isLoaded && (
