3156b4559002227a786f42b5654a9a05022d7eaa
Lukie Kang <lkang@bink.com>
2022-03-07 13:13:17 +0000

    less scale to fit

--- src/components/AssetGrid/AssetGrid.tsx
+++ src/components/AssetGrid/AssetGrid.tsx
@@ -45,7 +45,7 @@ const AssetGrid = ({planAssets}: Props) => {
       if (currentImage) {
         const {url} = currentImage
         return (
-          <div key={url} className='relative w-full h-[100px] grid items-center justify-center hover:scale-125 transition-all'>
+          <div key={url} className='relative w-full h-[100px] grid items-center justify-center hover:scale-115 transition-all'>
             <Asset
               image={currentImage}
               assetType={assetType}
@@ -68,7 +68,8 @@ const AssetGrid = ({planAssets}: Props) => {
   return (
     <div className='grid grid-cols-5 gap-2 grid-flow-col w-full text-center mt-[10px]'>
       <div className='flex flex-col'>{renderLabelColumnContents()}</div>
-      <div className='flex flex-col'>{dev?.length > 0 && renderAssetColumnContents('dev')}</div>
+      <div className='flex flex-col'>{staging?.length > 0 && renderAssetColumnContents('staging')}</div>
+      <div className='flex flex-col'>{staging?.length > 0 && renderAssetColumnContents('staging')}</div>
       <div className='flex flex-col'>{staging?.length > 0 && renderAssetColumnContents('staging')}</div>
     </div>
   )
