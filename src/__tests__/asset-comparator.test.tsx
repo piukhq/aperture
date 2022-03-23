a66d1a16e5dc4639eba2825c970f7d4c5272cd86
George <godonnell@bink.com>
2022-03-23 15:47:33 +0000

    Fleshed out unit tests for Asset Comparator

--- src/__tests__/asset-comparator.test.tsx
+++ src/__tests__/asset-comparator.test.tsx
@@ -1,20 +1,23 @@
 import React from 'react'
 import * as Redux from 'react-redux'
-import {render} from '@testing-library/react'
-import AssetComparatorPage from 'pages/asset-comparator'
+import {render, screen} from '@testing-library/react'
 import {Provider} from 'react-redux'
 import configureStore from 'redux-mock-store'
+import AssetComparatorPage from 'pages/asset-comparator'
+import * as WindowDimensions from 'utils/windowDimensions'
 
 jest.mock('components/AssetModal', () => () => null)
 jest.mock('components/CredentialsModal', () => () => null)
-const mockStoreFn = configureStore([])
-
+jest.mock('components/PlansList', () => () => null)
+jest.mock('components/AssetGrid', () => () => null)
 jest.mock('hooks/useGetPlansHook', () => ({
   useGetPlansHook: jest.fn(),
 }))
 
 describe('AssetComparatorPage', () => {
+  const mockStoreFn = configureStore([])
   const useDispatchMock = jest.spyOn(Redux, 'useDispatch')
+  const isDesktopViewportDimensionsMock = jest.spyOn(WindowDimensions, 'useIsDesktopViewportDimensions')
 
   let store
   const initialState = {
@@ -22,7 +25,7 @@ describe('AssetComparatorPage', () => {
       modalRequested: 'MODAL',
     },
     planAssets: {
-      selectedPlanImages: {},
+      selectedPlanImages: [],
     },
   }
 
@@ -31,6 +34,10 @@ describe('AssetComparatorPage', () => {
     store = mockStoreFn({...initialState})
     const dummyDispatch = jest.fn()
     useDispatchMock.mockReturnValue(dummyDispatch)
+    isDesktopViewportDimensionsMock.mockReturnValue(true)
+
+    // isVerified state value
+    React.useState = jest.fn().mockReturnValue([false, jest.fn()])
   })
 
   describe('Test Credentials Modal', () => {
@@ -90,7 +97,7 @@ describe('AssetComparatorPage', () => {
   })
 
   describe('Test headings', () => {
-    it('should render correct headings', () => {
+    it('should render the header container', () => {
       const {queryByTestId} = render(
         <Provider store={store}>
           <AssetComparatorPage />
@@ -99,5 +106,116 @@ describe('AssetComparatorPage', () => {
 
       expect(queryByTestId('header')).toBeInTheDocument()
     })
+
+    it('should render the Plan List', () => {
+      React.useState = jest.fn().mockReturnValue([true, jest.fn()])
+      const {queryByTestId} = render(
+        <Provider store={store}>
+          <AssetComparatorPage />
+        </Provider>
+      )
+
+      expect(queryByTestId('plan-list')).toBeInTheDocument()
+    })
+
+    it('should render Credentials button', () => {
+      const {getByRole} = render(
+        <Provider store={store}>
+          <AssetComparatorPage />
+        </Provider>
+      )
+
+      expect(getByRole('button', {name: /Credentials/i})).toBeInTheDocument()
+    })
+  })
+
+  describe('Test copy variations', () => {
+    it('should render small viewport copy', () => {
+      isDesktopViewportDimensionsMock.mockReturnValue(false)
+      const {getByText, queryByTestId} = render(
+        <Provider store={store}>
+          <AssetComparatorPage />
+        </Provider>
+      )
+
+      expect(queryByTestId('small-viewport-copy')).toBeInTheDocument()
+      expect(getByText('Viewport too small')).toBeInTheDocument()
+      expect(getByText('To use the asset comparator your browser window must be a minimum width of 1000px.')).toBeInTheDocument()
+      expect(getByText('Increase the size of your browser window to continue')).toBeInTheDocument()
+      expect(queryByTestId('unverified-landing-copy')).not.toBeInTheDocument()
+    })
+
+    it('should render unverified landing copy', () => {
+      const {getByText, queryByTestId} = render(
+        <Provider store={store}>
+          <AssetComparatorPage />
+        </Provider>
+      )
+
+      expect(queryByTestId('unverified-landing-copy')).toBeInTheDocument()
+      expect(getByText('Welcome to the Bink Asset Comparator')).toBeInTheDocument()
+      expect(getByText('Enter credentials above to compare assets across different environments')).toBeInTheDocument()
+      expect(queryByTestId('small-viewport-copy')).not.toBeInTheDocument()
+    })
+
+    describe('Test verified landing', () => {
+      beforeEach(() => {
+        React.useState = jest.fn().mockReturnValue([true, jest.fn()])
+      })
+
+      it('should render the environment headers', () => {
+        render(
+          <Provider store={store}>
+            <AssetComparatorPage />
+          </Provider>
+        )
+
+        const devHeading = screen.getByRole('heading', {
+          name: 'DEVELOPMENT',
+        })
+
+        const stagingHeading = screen.getByRole('heading', {
+          name: 'STAGING',
+        })
+
+        const sandboxHeading = screen.getByRole('heading', {
+          name: 'SANDBOX',
+        })
+
+        const prodHeading = screen.getByRole('heading', {
+          name: 'PRODUCTION',
+        })
+
+        expect(devHeading).toBeInTheDocument()
+        expect(stagingHeading).toBeInTheDocument()
+        expect(sandboxHeading).toBeInTheDocument()
+        expect(prodHeading).toBeInTheDocument()
+      })
+
+      it('should render verified landing copy', () => {
+        store = mockStoreFn({
+          ...initialState,
+          planAssets: {},
+        })
+        const {getByText} = render(
+          <Provider store={store}>
+            <AssetComparatorPage />
+          </Provider>
+        )
+
+        expect(getByText('Select a plan above to compare assets')).toBeInTheDocument()
+      })
+
+      it('should render Asset Grid', () => {
+        const {queryByTestId} = render(
+          <Provider store={store}>
+            <AssetComparatorPage />
+          </Provider>
+        )
+
+        expect(queryByTestId('asset-grid')).toBeInTheDocument()
+      })
+    })
+
   })
 })
--- src/pages/asset-comparator.tsx
+++ src/pages/asset-comparator.tsx
@@ -60,7 +60,11 @@ const AssetComparatorPage: NextPage = () => {
 
   const renderHeaderTools = () => (
     <>
-      { isVerified && <PlansList/>}
+      { isVerified && (
+        <div className='contents' data-testid='plan-list'>
+          <PlansList/>
+        </div>
+      )}
       <Button
         handleClick={handleRequestCredentialsModal}
         buttonSize={Button.buttonSize.MEDIUM_ICON}
@@ -68,13 +72,13 @@ const AssetComparatorPage: NextPage = () => {
         buttonBackground={Button.buttonBackground.BLUE}
         labelColour={Button.labelColour.WHITE}
         labelWeight={Button.labelWeight.MEDIUM}
-      > <SettingsSvg/>Credentials
+      ><SettingsSvg/>Credentials
       </Button>
     </>
   )
 
   const renderSmallViewportCopy = () => (
-    <div className='mt-[75px] flex flex-col items-center gap-6 text-left w-3/5'>
+    <div data-testid='small-viewport-copy' className='mt-[75px] flex flex-col items-center gap-6 text-left w-3/5'>
       <h1 className='font-heading-4 w-full'>Viewport too small</h1>
       <p className='font-subheading-3 w-full'>To use the asset comparator your browser window must be a minimum width of 1000px.</p>
       <p className='font-subheading-3 w-full'>Increase the size of your browser window to continue</p>
@@ -82,7 +86,7 @@ const AssetComparatorPage: NextPage = () => {
   )
 
   const renderUnverifiedLanding = () => (
-    <div className='mt-[115px] flex flex-col items-center text-center gap-4'>
+    <div data-testid='unverified-landing-copy' className='mt-[115px] flex flex-col items-center text-center gap-4'>
       <h1 className='font-heading-4'>Welcome to the Bink Asset Comparator</h1>
       <p className='font-subheading-3'>Enter credentials above to compare assets across different environments</p>
     </div>
@@ -90,7 +94,11 @@ const AssetComparatorPage: NextPage = () => {
 
   const renderVerifiedLanding = () => {
     const determineAssetGridStatus = () => (
-      planAssets ? <AssetGrid planAssets={planAssets} /> : <p className='col-span-5 mt-[42px] font-subheading-3'>Select a plan above to compare assets</p>
+      planAssets ? (
+        <div className='contents' data-testid='asset-grid'>
+          <AssetGrid planAssets={planAssets} />
+        </div>
+      ) : <p className='col-span-5 mt-[42px] font-subheading-3'>Select a plan above to compare assets</p>
     )
 
     return (
