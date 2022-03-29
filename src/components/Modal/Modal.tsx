097ae97af77c60749e9748f2b758d37acdf7aeeb
Lukie Kang <lkang@bink.com>
2022-03-30 00:27:22 +0100

    reformatting

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -19,7 +19,7 @@ const Modal = ({modalHeader, children}: Props) => {
 
   return (
     <FocusTrap>
-      <div className='min-h-screen'>
+      <div>
         <div className='fixed inset-0 bg-grey-975/[0.33] dark:bg-grey-200/[0.33] z-50' onClick={handleClose} />
         <div className='fixed left-2/4 translate-x-[-50%] h-screen justify-center z-50'>
           <div className='h-full w-[750px] py-[53px]'>
@@ -29,12 +29,9 @@ const Modal = ({modalHeader, children}: Props) => {
               </button>
             </div>
             <div aria-live='assertive' className='max-h-[calc(100%-61px)] rounded-b-[20px] px-[70px] overflow-y-scroll scrollbar-hidden bg-white dark:bg-grey-850'>
-              {modalHeader &&
-              <h1 className='mt-[19px] mb-[10px] font-heading-4'>{modalHeader}</h1>
-              }
+              {modalHeader && <h1 className='mt-[19px] mb-[10px] font-heading-4'>{modalHeader}</h1>}
               {children}
             </div>
-
           </div>
         </div>
       </div>
