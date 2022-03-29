c7fff5417d84578f60a1c5725433a8eec6e184af
Lukie Kang <lkang@bink.com>
2022-03-29 11:55:07 +0100

    hide scrollbar with additional border

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -21,7 +21,7 @@ const Modal = ({modalHeader, children}: Props) => {
     <FocusTrap>
       <div>
         <div className='fixed inset-0 bg-grey-975/[0.33] dark:bg-grey-200/[0.33] z-50' onClick={handleClose} />
-        <div className='fixed left-2/4 translate-x-[-50%] w-[750px] bg-white dark:bg-grey-850 rounded-[20px] justify-center mt-[53px] z-50'>
+        <div className='fixed left-2/4 translate-x-[-50%] max-h-[80%] w-[750px] bg-white dark:bg-grey-850 rounded-[20px] justify-center mt-[53px] z-50 border-white border-4 overflow-y-auto scrollbar-hidden'>
           <div className='flex h-[61px] flex-row-reverse items-center w-full border-b-[1px] border-grey-300 dark:border-grey-800'>
             <button className='mr-[29px]' aria-label='Close' onClick={handleClose}>
               <CloseIcon className='w-[14px] h-[14px]' />
