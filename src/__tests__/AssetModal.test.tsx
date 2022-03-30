6628f5b8c1609211486d27d6297d5c9fbf3e4e75
Lukie Kang <lkang@bink.com>
2022-03-30 10:05:33 +0100

    inital skeleton for test

---
+++ src/__tests__/AssetModal.test.tsx
@@ -0,0 +1,14 @@
+// import React from 'react'
+// import {render} from '@testing-library/react'
+
+// import {AssetModal} from 'components'
+
+describe('Asset Modal', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+  it('should render the heading', () => {
+    // render(<AssetModal />)
+    expect(true).toBeTruthy()
+  })
+})
