1e08784fa5fa1f306a07aa9898403be26626f2d6
George <godonnell@bink.com>
2022-04-01 09:40:06 +0100

    Fixed additional typo

--- src/__tests__/mids-database.test.tsx
+++ src/__tests__/mids-database.test.tsx
@@ -7,7 +7,7 @@ jest.mock('components/MerchantTile', () => () => <div data-testid='merchant-tile
 
 describe('MID Management DatabasePage', () => {
   describe('Test header content', () => {
-    it('should render correct heading and sun heading text', () => {
+    it('should render correct heading and sub heading text', () => {
       const {getByText} = render(<DatabasePage />)
       expect(getByText('MID Management')).toBeInTheDocument()
       expect(getByText('Create, view and manage MIDs for the merchants configured on the platform')).toBeInTheDocument()
