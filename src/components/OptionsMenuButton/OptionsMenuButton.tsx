35d931289b513769bb198d9ed0eb08d52c2e06a7
Lukie Kang <lkang@bink.com>
2022-04-19 11:28:38 +0100

    whitespace

--- src/components/OptionsMenuButton/OptionsMenuButton.tsx
+++ src/components/OptionsMenuButton/OptionsMenuButton.tsx
@@ -10,7 +10,6 @@ type Props = {
 
 const OptionsMenuButton = ({children}: Props) => {
   const buttonRef = useRef(null)
-
   const isElementBeyondRightViewportEdge = useIsElementBeyondRightViewportEdge(buttonRef, 280)
   const [isMenuOpen, setIsMenuOpen] = useState(false)
 
@@ -26,13 +25,15 @@ const OptionsMenuButton = ({children}: Props) => {
       </Button>
       {isMenuOpen && (
         <>
-          <div data-testid='options-menu'
+          <div
+            data-testid='options-menu'
             className={`flex flex-col gap-[10px] absolute rounded-[6px] border border-grey-200 dark:border-grey-800
          translate-y-[-40px] p-[15px] h-max w-max bg-white dark:bg-grey-850 justify-center z-40 shadow-[0_1px_5px_0px_rgba(0,0,0,0.4)]
          ${isElementBeyondRightViewportEdge ? 'translate-x-[-230px]' : 'translate-x-[50px]'}  `}>
             {children}
           </div>
-          <div className={`absolute h-[15px] w-[15px] bg-white dark:bg-grey-850 transform origin-top-left top-[20px] z-50 shadow-[-2px_-2px_3px_0px_rgba(0,0,0,0.15)] dark:shadow-[-1px_-1px_1px_0px_rgb(68,68,79)]
+          <div className={`absolute h-[15px] w-[15px] bg-white dark:bg-grey-850 transform origin-top-left top-[20px] z-50
+          shadow-[-2px_-2px_3px_0px_rgba(0,0,0,0.15)] dark:shadow-[-1px_-1px_1px_0px_rgb(68,68,79)]
           ${isElementBeyondRightViewportEdge ? 'rotate-[135deg] right-[27px]' : 'rotate-[-45deg] right-[-17px]'}`}></div>
           <div className='fixed inset-0 z-30' onClick={() => setIsMenuOpen(false)} />
         </>
--- src/components/OptionsMenuButton/components/OptionsMenuItem.tsx
+++ src/components/OptionsMenuButton/components/OptionsMenuItem.tsx
@@ -1,6 +1,5 @@
 import React, {ReactNode} from 'react'
 
-
 type Props = {
   icon: ReactNode,
   label: string,
