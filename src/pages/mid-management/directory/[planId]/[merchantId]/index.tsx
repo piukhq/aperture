726f9b2118d1561a23d464e6aac9d2ed00140c71
Lukie Kang <lkang@bink.com>
2022-05-12 15:17:39 +0100

    add initial tab navigation

--- src/pages/mid-management/directory/[planId]/[merchantId]/index.tsx
+++ src/pages/mid-management/directory/[planId]/[merchantId]/index.tsx
@@ -1,3 +1,4 @@
+import {useState} from 'react'
 import type {NextPage} from 'next'
 import {useRouter} from 'next/router'
 import {PageLayout, DirectoryDetailsHeader, DirectoryMerchantMids, DirectoryMidModal} from 'components'
@@ -14,7 +15,14 @@ const MerchantDetailsPage: NextPage = () => {
   const selectedPlan = useAppSelector(getSelectedDirectoryPlan)
   const selectedMerchant = useAppSelector(getSelectedDirectoryMerchant)
   const modalRequested: ModalType = useAppSelector(selectModal)
-  const {merchantId} = router.query
+  const {merchantId, planId, tab} = router.query
+
+  enum TabMenu {
+    MIDS = 'mids',
+    SECONDARY_MIDS ='secondary-mids',
+    LOCATIONS = 'locations',
+    IDENTIFIERS ='identifiers'
+  }
 
   const getPlanDetails = () => {
     return mockPlanDetailsData
@@ -30,13 +38,26 @@ const MerchantDetailsPage: NextPage = () => {
   const {name, icon_url: iconUrl, location_label: locationLabel} = merchant.merchant_metadata
 
 
+  const handleNavigationClick = (selectedTab) => {
+    router.replace(`/mid-management/directory/${planId}/${merchantId}?tab=${selectedTab}`)
+  }
+
   const renderMerchantDetailsSection = () => {
-    // TODO: Add logic to render different future components based on the tab query string
-    return <DirectoryMerchantMids />
+    if (tab === TabMenu.MIDS) {
+      return <DirectoryMerchantMids />
+    } else if (tab === TabMenu.LOCATIONS) {
+      return null // TODO: Swap out with future locations component
+    } else if (tab === TabMenu.SECONDARY_MIDS) {
+      return null // TODO: Swap out with future secondary mids component
+    } else if (tab === TabMenu.IDENTIFIERS) {
+      return <DirectoryMerchantMids/>
+    }
   }
 
   const tabSelectedClasses = 'font-heading-8 text-grey-900 dark:text-grey-100 bg-white dark:bg-grey-825 dark:hover:text-white border-b-2 border-b-blue'
-  const tabUnselectedClasses = 'font-heading-8 font-regular text-sm text-grey-600 dark:text-grey-100 bg-white dark:bg-grey-825 dark:hover:text-white  hover:text-grey-900 duration-200'
+  const tabUnselectedClasses = 'font-heading-8 font-regular text-sm text-grey-600 dark:text-grey-400 bg-white dark:bg-grey-825 dark:hover:text-white  hover:text-grey-900 duration-200'
+
+  const checkTabSelection = (navigationTab: string) => navigationTab === tab ? tabSelectedClasses : tabUnselectedClasses
 
   return (
     <>
@@ -45,16 +66,16 @@ const MerchantDetailsPage: NextPage = () => {
         <DirectoryDetailsHeader planId={schemeId} name={name} slug={slug} iconUrl={iconUrl} locationLabel={locationLabel} isMerchant />
         <div className='rounded-[10px] mt-[15px] bg-white dark:bg-grey-825 shadow-[0_1px_6px_0px_rgba(0,0,0,0.5)]'>
           <nav className='grid grid-cols-4 w-full pl-[69px] border-b border-grey-800/10 pr-[10px]'>
-            <button className={tabSelectedClasses}>
+            <button className={checkTabSelection(TabMenu.MIDS)} onClick={() => handleNavigationClick(TabMenu.MIDS)}>
               <span className='place-content-center flex h-[51px] items-center'>MIDs</span>
             </button>
-            <button className={tabUnselectedClasses}>
+            <button className={checkTabSelection(TabMenu.SECONDARY_MIDS)} onClick={() => handleNavigationClick(TabMenu.SECONDARY_MIDS)}>
               <span className='place-content-center flex h-[51px] items-center '>Secondary MIDs</span>
             </button>
-            <button className={tabUnselectedClasses}>
+            <button className={checkTabSelection(TabMenu.LOCATIONS)} onClick={() => handleNavigationClick(TabMenu.LOCATIONS)}>
               <span className='place-content-center flex h-[51px] items-center'>Locations</span>
             </button>
-            <button className={tabUnselectedClasses}>
+            <button className={checkTabSelection(TabMenu.IDENTIFIERS)} onClick={() => handleNavigationClick(TabMenu.IDENTIFIERS)}>
               <span className='place-content-center flex h-[51px] items-center'>Identifiers</span>
             </button>
           </nav>
