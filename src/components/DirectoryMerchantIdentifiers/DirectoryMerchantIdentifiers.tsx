b951df32a727181a998b9dc97e5a1072f9f6f590
Lukie Kang <lkang@bink.com>
2022-05-12 23:29:40 +0100

    complete DirectoryMerchantIdentifiers component with mock data and types

--- src/components/DirectoryMerchantIdentifiers/DirectoryMerchantIdentifiers.tsx
+++ src/components/DirectoryMerchantIdentifiers/DirectoryMerchantIdentifiers.tsx
@@ -1,35 +1,25 @@
-import {mockMidsData} from 'utils/mockMidsData'
-import {DirectoryMids} from 'types'
-import {ModalType, PaymentSchemeName} from 'utils/enums'
-import {
-  useAppDispatch,
-} from 'app/hooks'
-import {requestModal} from 'features/modalSlice'
-import {setSelectedDirectoryMidPaymentScheme} from 'features/directoryMidSlice'
+import {mockIdentifiersData} from 'utils/mockIdentifiersData'
+import {DirectoryIdentifiers} from 'types'
+
 import AddVisaSvg from 'icons/svgs/add-visa.svg'
 import AddMastercardSvg from 'icons/svgs/add-mastercard.svg'
-import AddAmexSvg from 'icons/svgs/add-amex.svg'
-import AmexSvg from 'icons/svgs/amex-logo-small.svg'
 import MastercardSvg from 'icons/svgs/mastercard-logo-small.svg'
 import VisaSvg from 'icons/svgs/visa-logo-small.svg'
 
 const DirectoryMerchantIdentifiers = () => {
-  const midsData: DirectoryMids = mockMidsData
-  const dispatch = useAppDispatch()
+  const identifiersData: DirectoryIdentifiers = mockIdentifiersData
 
   const renderPaymentSchemeLogo = (index:number, paymentSchemeCode) => {
     if (paymentSchemeCode === 1) {
       return <VisaSvg key={index} alt='Visa'/>
     } else if (paymentSchemeCode === 2) {
       return <MastercardSvg key={index} alt='Mastercard' />
-    } else if (paymentSchemeCode === 3) {
-      return <AmexSvg key={index} alt='Amex'/>
     }
   }
 
-  const renderRow = (midElement, index) => {
-    const {date_added: dateAdded, mid_metadata: midMetadata} = midElement
-    const {payment_scheme_code: paymentSchemeCode, mid, visa_bin: visaBin} = midMetadata
+  const renderRow = (identifierElement, index) => {
+    const {date_added: dateAdded, identifier_metadata: identifierMetadata} = identifierElement
+    const {value, payment_scheme_merchant_name: paymentSchemeMerchantName, payment_scheme_code: paymentSchemeCode} = identifierMetadata
 
     return (
       <tr className='h-[60px]' key={index}>
@@ -39,58 +29,46 @@ const DirectoryMerchantIdentifiers = () => {
         <td>
           {renderPaymentSchemeLogo(index, paymentSchemeCode)}
         </td>
-        <td className='px-[9px] font-heading-8 font-regular  truncate'>{mid}</td>
-        <td className='px-[9px] font-body-3 truncate'>{visaBin}</td>
+        <td className='px-[9px] font-heading-8 font-regular truncate'>{value}</td>
+        <td className='px-[9px] font-body-3 truncate'>{paymentSchemeMerchantName}</td>
         <td className='px-[9px] font-body-3 truncate'>{dateAdded}</td>{/* TODO: Will need formatting when coming from API */ }
-        <td className='px-[9px]'></td>
-        <td className='px-[9px]'></td>
+        <td className='px-[9px]'></td> {/* TODO: Add Harmonia Status */}
       </tr>
     )
-  }
-
-  const requestMidModal = (paymentScheme) => {
-    dispatch(setSelectedDirectoryMidPaymentScheme(paymentScheme))
-    dispatch(requestModal(ModalType.MID_MANAGEMENT_DIRECTORY_MID))
   }
 
   return (
     <div className='mx-[10px]'>
       <div className='flex h-[71px] items-center justify-end pr-[9px]'>
         <button
-          onClick={() => requestMidModal(PaymentSchemeName.VISA)}
-          aria-label='Add Visa MID'
+          onClick={() => console.log('Placeholder: Request Visa Identifier')}
+          aria-label='Add Visa Identifier'
         ><AddVisaSvg alt=''/>
         </button>
         <button
-          onClick={() => requestMidModal(PaymentSchemeName.MASTERCARD)}
-          aria-label='Add Mastercard MID'
+          onClick={() => console.log('Placeholder: Request Mastercard Identifier')}
+          aria-label='Add Mastercard Identifier'
         ><AddMastercardSvg alt=''/>
         </button>
-        <button
-          onClick={() => requestMidModal(PaymentSchemeName.AMEX)}
-          aria-label='Add Amex MID'
-        ><AddAmexSvg alt=''/>
-        </button>
       </div>
-jiojioijojio
+
       <table className='w-full min-w-[200px] rounded-[10px] bg-white dark:bg-grey-825 table-fixed'>
         <thead className='h-[38px] text-left bg-grey-200'>
           <tr>
-            <th data-testid='table-header' aria-label='group-checkbox' className='px-[9px] w-[40px] rounded-tl-[10px] rounded-bl-[10px]'>
+            <th data-testid='table-header' aria-label='group-checkbox' className='px-[9px] w-[40px] rounded-l-[10px]'>
               <div className='flex items-center justify-center'>
                 <input type='checkbox' className='flex h-[16px] w-[16px]' onChange={() => console.log('Header Checkbox clicked')}/>
               </div>
             </th>
-            <th data-testid='table-header' aria-label='payment-scheme' className='px-[9px] w-[40px]'></th>
-            <th data-testid='table-header' className='px-[9px] font-table-header text-grey-800'>VALUE</th>
-            <th data-testid='table-header' className='px-[9px] font-table-header text-grey-800'>BIN</th>
-            <th data-testid='table-header' className='px-[9px] font-table-header text-grey-800'>DATE ADDED</th>
-            <th data-testid='table-header' className='px-[9px] font-table-header text-grey-800'>SCHEME STATUS</th>
-            <th data-testid='table-header' className='px-[9px] font-table-header text-grey-800 rounded-tr-[10px] rounded-br-[10px]'>HARMONIA STATUS</th>
+            <th data-testid='table-header' aria-label='payment-scheme' className='px-[9px] w-[50px]'></th>
+            <th data-testid='table-header' className='px-[9px] font-table-header w-[160px]'>VALUE</th>
+            <th data-testid='table-header' className='px-[9px] font-table-header'>SCHEME NAME</th>
+            <th data-testid='table-header' className='px-[9px] font-table-header'>DATE ADDED</th>
+            <th data-testid='table-header' className='px-[9px] font-table-header rounded-r-[10px]'>HARMONIA STATUS</th>
           </tr>
         </thead>
         <tbody>
-          {midsData.map((mid, index) => renderRow(mid, index))}
+          {identifiersData.map((identifier, index) => renderRow(identifier, index))}
         </tbody>
       </table>
     </div>
--- src/types/index.ts
+++ src/types/index.ts
@@ -134,12 +134,25 @@ export type DirectoryMid = {
   date_added: string // TODO: Change this depending on API value
   txm_status: string // TODO: could be an enum, union type
 }
-
 export type DirectoryMidMetadata = {
   payment_scheme_code: number,
   mid: string,
   visa_bin?: string,
   payment_enrolment_status: string // TODO: could be an enum/union type
+}
+
+export type DirectoryIdentifiers = Array<DirectoryIdentifier>
+
+export type DirectoryIdentifier = {
+  identifier_ref: string,
+  identifier_metadata: DirectoryIdentifierMetadata,
+  date_added: string // TODO: Change this depending on API value
+}
+
+export type DirectoryIdentifierMetadata = {
+  value: string,
+  payment_scheme_merchant_name: string,
+  payment_scheme_code: number
 }
 
 export type OptionsMenuItem = {
