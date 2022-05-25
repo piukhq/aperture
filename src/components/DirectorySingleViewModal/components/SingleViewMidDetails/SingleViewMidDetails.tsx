5ea5182e93ddf01fc038573bf2529e34534e79a3
Lukie Kang <lkang@bink.com>
2022-05-26 00:13:23 +0100

    add dropdown component

--- src/components/DirectorySingleViewModal/components/SingleViewMidDetails/SingleViewMidDetails.tsx
+++ src/components/DirectorySingleViewModal/components/SingleViewMidDetails/SingleViewMidDetails.tsx
@@ -1,12 +1,15 @@
-import {Button} from 'components'
+import {Button, Dropdown} from 'components'
 import {ButtonType, ButtonWidth, ButtonSize, ButtonBackground, LabelColour, LabelWeight} from 'components/Button/styles'
 import {DirectoryMid} from 'types'
 import {getSelectedDirectoryMerchantEntity} from 'features/directoryMerchantSlice'
 import {useAppSelector} from 'app/hooks'
 import {PaymentSchemeCode} from 'utils/enums'
+import {useState} from 'react'
 
 
 const SingleViewMidDetails = () => {
+  const displayValues = ['Not enrolled']
+  const [displayValue, setDisplayValue] = useState(displayValues[0])
   const selectedEntity = useAppSelector(getSelectedDirectoryMerchantEntity)
   const {date_added: dateAdded, mid_metadata: midMetadata, txm_status: txmStatus}: DirectoryMid = selectedEntity
   const {payment_scheme_code: paymentSchemeCode, visa_bin: visaBin} = midMetadata
@@ -34,21 +37,15 @@ const SingleViewMidDetails = () => {
         </div>
         <section>
           <div className='flex flex-col'>
-            <h2 className='font-single-view-heading'>PAYMENT SCHEME STATUS</h2>
-
-
-            <select className='font-body-3 font-medium text-grey-600 w-[148px] bg-transparent rounded-[10px] border-[1px] border-grey-300 h-[28px] px-[10px] my-[5px]'>
-              <option className='font-body-3 font-medium text-grey-600 '>Not enrolled</option>
-
-
-            </select>
+            <label className='font-single-view-heading pl-[15px]'>PAYMENT SCHEME STATUS</label>
+            <Dropdown displayValue={displayValue} displayValues={displayValues} onChangeDisplayValue={setDisplayValue} />
           </div>
         </section>
       </div>
       <section className=' h-[38px] flex justify-between mb-[34px]'>
         <div>
           <h2 className='font-single-view-heading'>LOCATION </h2>
-          <p className='font-single-view-data'>Unknown</p> {/* TODO: Set location */}
+          <p className='font-single-view-data'>Unknown</p> {/* TODO: Set location...how exactly? */}
         </div>
         <Button
           buttonType={ButtonType.SUBMIT}
