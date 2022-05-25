e2a469785cec661862724c04a7b834c590f71bc6
George <godonnell@bink.com>
2022-05-25 15:47:21 +0100

    Added functionality around the New MID model

--- src/hooks/useMidManagementMerchants.ts
+++ src/hooks/useMidManagementMerchants.ts
@@ -1,8 +1,9 @@
-import {usePostMerchantMutation, useDeleteMerchantMutation} from 'services/midManagementMerchants'
+import {usePostMerchantMutation, useDeleteMerchantMutation, usePostMerchantMidMutation} from 'services/midManagementMerchants'
 
 export const useMidManagementMerchants = () => {
   const [postMerchant, {data: postMerchantResponse, isLoading: postMerchantIsLoading, error: postMerchantError, reset: resetPostMerchantResponse}] = usePostMerchantMutation({fixedCacheKey: 'postMerchant'})
   const [deleteMerchant, {isSuccess: deleteMerchantIsSuccess, isLoading: deleteMerchantIsLoading, error: deleteMerchantError, reset: resetDeleteMerchantResponse}] = useDeleteMerchantMutation({fixedCacheKey: 'deleteMerchant'})
+  const [postMerchantMid, {data: postMerchantMidResponse, isLoading: postMerchantMidIsLoading, error: postMerchantMidError, reset: resetPostMerchantMidResponse}] = usePostMerchantMidMutation({fixedCacheKey: 'postMerchantMid'})
 
   return {
     postMerchant,
@@ -15,5 +16,10 @@ export const useMidManagementMerchants = () => {
     deleteMerchantIsLoading,
     deleteMerchantError,
     resetDeleteMerchantResponse,
+    postMerchantMid,
+    postMerchantMidResponse,
+    postMerchantMidIsLoading,
+    postMerchantMidError,
+    resetPostMerchantMidResponse,
   }
 }
