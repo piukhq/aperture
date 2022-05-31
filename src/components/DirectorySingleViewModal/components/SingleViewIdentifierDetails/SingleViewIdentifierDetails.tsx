c8ad3bc0c3097781350de31e36ebb484d747522b
Lukie Kang <lkang@bink.com>
2022-05-31 09:41:01 +0100

    implement identifier modal

---
+++ src/components/DirectorySingleViewModal/components/SingleViewIdentifierDetails/SingleViewIdentifierDetails.tsx
@@ -0,0 +1,55 @@
+import {Button} from 'components'
+import {ButtonType, ButtonWidth, ButtonSize, ButtonBackground, LabelColour, LabelWeight} from 'components/Button/styles'
+import {DirectoryIdentifier} from 'types'
+import {getSelectedDirectoryMerchantEntity} from 'features/directoryMerchantSlice'
+import {useAppSelector} from 'app/hooks'
+import {PaymentSchemeCode, PaymentSchemeStartCaseName} from 'utils/enums'
+
+const SingleViewIdentifierDetails = () => {
+  const selectedEntity = useAppSelector(getSelectedDirectoryMerchantEntity) as DirectoryIdentifier
+  const {date_added: dateAdded, identifier_metadata: identifierMetadata, identifier_status: identifierStatus} = selectedEntity
+  const {payment_scheme_code: paymentSchemeCode} = identifierMetadata
+
+  const getPaymentScheme = () => {
+    if (paymentSchemeCode === PaymentSchemeCode.VISA) {
+      return PaymentSchemeStartCaseName.VISA
+    } else if (paymentSchemeCode === PaymentSchemeCode.MASTERCARD) {
+      return PaymentSchemeStartCaseName.MASTERCARD
+    } else if (paymentSchemeCode === PaymentSchemeCode.AMEX) {
+      return PaymentSchemeStartCaseName.AMEX
+    }
+  }
+
+  return (
+    <>
+      <div className='mb-[34px]'>
+        <h2 className='font-single-view-heading'>DATE ADDED</h2>
+        <p className='font-single-view-data'>{dateAdded}</p>
+      </div>
+      <section className='mb-[34px] grid grid-cols-2 h-[50px]'>
+        <div>
+          <h2 className='font-single-view-heading'>PAYMENT SCHEME</h2>
+          <p className='font-single-view-data'>{getPaymentScheme()}</p>
+        </div>
+      </section>
+      <section className='h-[38px] flex justify-between mb-[34px]'>
+        <div>
+          <h2 className='font-single-view-heading'>HARMONIA STATUS</h2>
+          <p className='font-single-view-data'>{identifierStatus}</p>
+        </div>
+        <Button
+          buttonType={ButtonType.SUBMIT}
+          buttonSize={ButtonSize.MEDIUM}
+          buttonWidth={ButtonWidth.MEDIUM}
+          buttonBackground={ButtonBackground.LIGHT_GREY}
+          labelColour={LabelColour.GREY}
+          labelWeight={LabelWeight.SEMIBOLD}
+        >Edit
+        </Button>
+      </section>
+    </>
+  )
+}
+export default SingleViewIdentifierDetails
+
+
