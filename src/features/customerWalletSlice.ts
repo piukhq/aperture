1bd452ffc6b5c99b9940fae49703f421d0cdaf16
Lukie Kang <lkang@bink.com>
2022-06-08 07:41:22 +0100

    remove unneeded slice stuff

--- src/features/customerWalletSlice.ts
+++ src/features/customerWalletSlice.ts
@@ -3,14 +3,10 @@ import {RootState} from 'app/store'
 
 type CustomerWallet = {
   jwtToken: string,
-  loyaltyCards: any, // TODO Temporary any
-  paymentCards: any, // TODO Temporary any
 }
 
 const initialState: CustomerWallet = {
   jwtToken: null,
-  loyaltyCards: [],
-  paymentCards: [],
 }
 
 export const customerWalletSlice = createSlice({
@@ -20,20 +16,12 @@ export const customerWalletSlice = createSlice({
     setJwtToken: (state, action: PayloadAction<string>) => {
       state.jwtToken = action.payload
     },
-    setLoyaltyCards: (state, action) => {
-      state.loyaltyCards = action.payload
-    },
-    setPaymentCards: (state, action) => {
-      state.paymentCards = action.payload
-    },
     reset: () => initialState,
   },
 })
 
-export const {setJwtToken, setLoyaltyCards, setPaymentCards, reset} = customerWalletSlice.actions
+export const {setJwtToken, reset} = customerWalletSlice.actions
 
 export const getCustomerWallet = (state: RootState) => state.customerWallet
 export const getJwtToken = (state: RootState) => state.customerWallet.jwtToken
-export const getLoyaltyCards = (state: RootState) => state.customerWallet.loyaltyCards
-export const getPaymentCards = (state: RootState) => state.customerWallet.paymentCards
 export default customerWalletSlice.reducer
