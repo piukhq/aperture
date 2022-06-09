9b52a17297fe85cc1ce59d054868432b507f2eb0
Lukie Kang <lkang@bink.com>
2022-06-09 15:19:07 +0100

    add link status

--- src/components/CustomerWallet/CustomerWallet.tsx
+++ src/components/CustomerWallet/CustomerWallet.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React from 'react'
 import Image from 'next/image'
 
 import {useCustomerWallet} from 'hooks/useCustomerWallet'
@@ -6,18 +6,13 @@ import MastercardSvg from 'icons/svgs/mastercard-logo-small.svg'
 import VisaSvg from 'icons/svgs/visa-logo-small.svg'
 import AmexSvg from 'icons/svgs/amex-logo-small.svg'
 import ArrowDownSvg from 'icons/svgs/arrow-down.svg'
+import CheckSvg from 'icons/svgs/check.svg'
+import CloseSvg from 'icons/svgs/close.svg'
+
 
 const CustomerWallet = () => {
-  const [customerPlans, setCustomerPlans] = useState(null)
   const {getLoyaltyCardsResponse, getPaymentCardsResponse, getPlansResponse} = useCustomerWallet()
 
-  console.log
-  useEffect(() => {
-    if (getPlansResponse && getLoyaltyCardsResponse) {
-      const loyaltyCardPlans = getLoyaltyCardsResponse.map((loyaltyCard) => loyaltyCard.membership_plan)
-      setCustomerPlans(getPlansResponse.filter((plan) => loyaltyCardPlans.includes(plan.id)))
-    }
-  }, [getPlansResponse, getLoyaltyCardsResponse])
 
   const renderPaymentSchemeLogo = (paymentSchemeName: string) => {
     if (paymentSchemeName === 'Visa') {
@@ -42,9 +37,9 @@ const CustomerWallet = () => {
   }
 
   const renderPaymentCards = () => (
-    <div className={`w-full grid grid-cols-${getPaymentCardsResponse?.length} gap-[15px]`}>
+    <div className={`grid grid-cols-${getPaymentCardsResponse?.length} gap-[15px]`}>
       {getPaymentCardsResponse?.map((paymentCard) => (
-        <div key={paymentCard.id} className='h-[52px] max-w-[200px] min-w-[135px] shadow-md flex gap-1 p-[6px] rounded-[8px] items-center space-between'>
+        <div key={paymentCard.id} className='h-[52px] w-[200px] shadow-md flex gap-1 p-[6px] rounded-[8px] items-center space-between'>
           <div className='basis-1/5'>
             {renderPaymentSchemeLogo(paymentCard.card.provider)}
           </div>
@@ -63,30 +58,45 @@ const CustomerWallet = () => {
     </div>
   )
 
-
-  const getPlanName = (planId: number) => customerPlans?.find((plan) => plan.id === planId)?.account.plan_name
-
+  const getLinkStatus = (loyaltyCardPaymentCards, paymentCardIndex) => {
+    const paymentCardId = getPaymentCardsResponse[paymentCardIndex].id
+    const isPaymentCardLinkedToThisLoyaltyCard = loyaltyCardPaymentCards.some((loyaltyCardPaymentCard) => loyaltyCardPaymentCard.id === paymentCardId)
+    return isPaymentCardLinkedToThisLoyaltyCard ? <CheckSvg fill='green' className='scale-125' /> : <CloseSvg fill='red' className={'h-[15px]'} />
+  }
   const renderLoyaltyIcon = (images) => {
     const icon = images.find((image) => image.type === 3)
     return icon && <Image className='rounded-[5px]' src={icon.url} height={40} width={40} alt=''/>
   }
 
-  const getCurrentVoucher = (vouchers) => {
-    const currentVoucher = vouchers.find((voucher) => voucher.state === 'inprogress')
-    return currentVoucher ? `${currentVoucher.earn.value} ${currentVoucher.earn.suffix}` : 'N/A'
+  const getBalanceString = (loyaltyCard, plan) => {
+    if (plan.slug === 'iceland-bonus-card') {
+      return `${loyaltyCard.balances[0].prefix}${loyaltyCard.balances[0].value} spent`
+    }
+
+    const currentVoucherEarn = loyaltyCard.vouchers?.find((voucher) => voucher.state === 'inprogress').earn
+
+    if (currentVoucherEarn?.type === 'stamps') {
+      return `${currentVoucherEarn.value} ${currentVoucherEarn.suffix}`
+    } else if (currentVoucherEarn?.type === 'accumulator') {
+      return `${currentVoucherEarn.value} / ${currentVoucherEarn.target_value} spent`
+    }
+    return 'N/A'
   }
 
 
   const renderLoyaltyCardsRow = () => (
     <div>
       {getLoyaltyCardsResponse?.map((loyaltyCard) => {
-        const {id, status, images, vouchers, membership_plan} = loyaltyCard
+        const plan = getPlansResponse?.find((plan) => plan.id === loyaltyCard.membership_plan)
+        const isPllCard = plan?.feature_set.card_type === 2
+        const {id, status, images, payment_cards: paymentCards} = loyaltyCard
+
         return (
-          <div key={id} className='h-[90px] mb-[15px]'>
-            <div className='flex h-[90px] w-[180px] shadow-md rounded-[8px] items-center p-[6px]'>
+          <div key={id} className='flex space-between mb-[17px]'>
+            <div className='flex h-[90px] min-w-[180px] shadow-md rounded-[8px] items-center p-[6px]'>
               <div className='flex flex-col h-full space-between basis-4/5 font-body-4'>
                 <p className='font-bold flex gap-[6px] items-center h-full leading-snug'>
-                  {getPlanName(membership_plan)}
+                  {plan?.account.plan_name}
                   <span className={`flex items-center justify-center h-[6px] min-w-[6px] rounded-full bg-${getStatusColour(status.state)}`}/>
                 </p>
                 <div className='flex gap-[7px]'>
@@ -95,13 +105,21 @@ const CustomerWallet = () => {
                   </div>
                   <div className='basis-3/4 leading-snug'>
                     <p>{id}</p>
-                    <p>{vouchers ? getCurrentVoucher(vouchers) : 'N/A'}</p>
+                    <p>{isPllCard ? getBalanceString(loyaltyCard, plan) : 'N/A'}</p>
                   </div>
                 </div>
               </div>
               <div className='basis-1/5 flex justify-end'>
                 <ArrowDownSvg className={'-rotate-90 scale-75'} />
               </div>
+            </div>
+
+            <div key={id} className={`grid grid-cols-${getPaymentCardsResponse?.length} gap-[15px]`}>
+              {getPaymentCardsResponse?.map((paymentCard, index) => (
+                <div key={paymentCard.id} className='w-[200px] flex items-center justify-center'>
+                  { isPllCard ? getLinkStatus(paymentCards, index) : <div className='h-[3px] bg-grey-600 w-[20px]'/>}
+                </div>
+              ))}
             </div>
           </div>
         )
@@ -110,24 +128,21 @@ const CustomerWallet = () => {
 
   )
   return (
-    <div className={'bg-white min-h-[400px] min-w-[900px] shadow-md rounded-[20px] p-[20px]'}>
-      <div className={'h-[92px] pl-[180px] w-full flex'}>
+    <div className={'bg-white min-h-[400px] min-w-[900px] shadow-md rounded-[20px] p-[20px] flex flex-col justify-center'}>
+      <div className={'h-[92px] pl-[180px] flex'}>
         {renderPaymentCards()}
       </div>
       {renderLoyaltyCardsRow()}
     </div>
-
-
   )
 }
 
 export default CustomerWallet
 
 
-// 3. PLAN NAME?!
-// status check.
-// Linked elsewhere variants
-// Responsive design
+// Linked elsewhere card variants
+
+// Refactor into components
+// change jwt
 // Dark mode
-
-
+// Wierd dot thing asnd into ticks
