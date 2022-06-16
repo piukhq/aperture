ca6ccb50ddcb3bd821da844833ad038cb38a19fe
Lukie Kang <lkang@bink.com>
2022-06-16 13:37:51 +0100

    remove unneeded slice for now

--- src/features/customerWalletSlice.ts
+++ src/features/customerWalletSlice.ts
@@ -1,15 +1,12 @@
 import {createSlice, PayloadAction} from '@reduxjs/toolkit'
 import {RootState} from 'app/store'
-import {DecodedUserToken} from 'types'
 
 type CustomerWallet = {
   jwtToken: string,
-  decodedJwtToken: DecodedUserToken,
 }
 
 const initialState: CustomerWallet = {
   jwtToken: null,
-  decodedJwtToken: null,
 }
 
 export const customerWalletSlice = createSlice({
@@ -19,16 +16,12 @@ export const customerWalletSlice = createSlice({
     setJwtToken: (state, action: PayloadAction<string>) => {
       state.jwtToken = action.payload
     },
-    setDecodedJwtToken: (state, action: PayloadAction<DecodedUserToken>) => {
-      state.decodedJwtToken = action.payload
-    },
     reset: () => initialState,
   },
 })
 
-export const {setJwtToken, setDecodedJwtToken, reset} = customerWalletSlice.actions
+export const {setJwtToken, reset} = customerWalletSlice.actions
 
 export const getCustomerWallet = (state: RootState) => state.customerWallet
 export const getJwtToken = (state: RootState) => state.customerWallet.jwtToken
-export const getDecodedJwtToken = (state: RootState) => state.customerWallet.decodedJwtToken
 export default customerWalletSlice.reducer
