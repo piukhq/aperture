dadce108ccd2a7b9c51d72d613fd3f61b695f960
Lukie Kang <lkang@bink.com>
2022-07-28 16:20:43 +0100

    Add comment

--- src/hooks/useCustomerLookup.ts
+++ src/hooks/useCustomerLookup.ts
@@ -20,6 +20,7 @@ export const useCustomerLookup = () => {
   const dispatch = useDispatch()
   const jwtToken = useAppSelector(getJwtToken)
 
+  // TODO: Consider refactoring this to avoid using the useEffect hook
   useEffect(() => { // TODO: Make this lookup type agnostic when other lookups are implemented
     if (lookupType && jwtToken) {
       const {bundle_id: channel, sub: userId, user_id: userEmail} = decodeJwtToken(jwtToken) || {}
