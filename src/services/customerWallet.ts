af214c693f702219c06316754b73493cb9b2aa4d
Lukie Kang <lkang@bink.com>
2022-08-03 15:53:15 +0100

    Add Bearer or Token switch depending on if Barclays or not

--- src/services/customerWallet.ts
+++ src/services/customerWallet.ts
@@ -1,14 +1,17 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 import {LoyaltyCard, PaymentCard, Plan, Service} from 'types'
 import type {RootState} from 'app/store'
-import {UrlEndpoint} from 'utils/enums'
+import {BundleID, UrlEndpoint} from 'utils/enums'
+import {decodeJwtToken} from 'utils/jwtToken'
 
 const baseQuery = fetchBaseQuery({
   baseUrl: process.env.NEXT_PUBLIC_LOYALTY_API_URL,
   prepareHeaders: (headers, {getState}) => {
     const token = (getState() as RootState).customerWallet.jwtToken
+    const channel = decodeJwtToken(token).bundle_id
+
     if (token) {
-      headers.set('authorization', `Token ${token}`)
+      headers.set('authorization', `${channel === BundleID.BARCLAYS_BUNDLE_ID ? 'Bearer' : 'Token'} ${token}`)
       headers.set('accept', 'application/json;v=1.3')
     }
     return headers
