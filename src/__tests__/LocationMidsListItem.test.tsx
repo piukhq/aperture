9dc4cac1fb19b455358ce23c429092d89048abae
Lukie Kang <lkang@bink.com>
2022-08-09 15:04:10 +0100

    add unit tests for LocationMidsListItem

--- src/__tests__/LocationMidsListItem.test.tsx
+++ src/__tests__/LocationMidsListItem.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {render, screen} from '@testing-library/react'
+import {fireEvent, render, screen} from '@testing-library/react'
 import LocationMidsListItem from 'components/DirectorySingleViewModal/components/SingleViewLocation/components/LocationMidsListItem'
 
 jest.mock('components/DirectorySingleViewModal/components/SingleViewLocation/components/PaymentCardIcon', () => () => <div data-testid='payment-card-icon' />)
@@ -43,5 +43,56 @@ describe('LocationMidsListItem', () => {
     render(getLocationMidsListItemComponent())
     expect(screen.getByLabelText(`View ${mockRefValue}`)).toBeInTheDocument()
     expect(screen.getByLabelText(`Unlink ${mockRefValue}`)).toBeInTheDocument()
+  })
+
+  it('should call the setUnlinkingMidFn with correct index when the unlink button is clicked', () => {
+    render(getLocationMidsListItemComponent())
+    fireEvent.click(screen.getByLabelText(`Unlink ${mockRefValue}`))
+    expect(mockProps.setUnlinkingMidFn).toHaveBeenCalledWith(mockProps.index)
+  }),
+
+  describe('Test unlinking confirmation state', () => {
+
+    const getUnlinkingConfirmingStateComponent = getLocationMidsListItemComponent({
+      isInUnlinkingConfirmationState: true,
+    })
+
+    it('should render the unlink confirmation button', () => {
+      render(getUnlinkingConfirmingStateComponent)
+      expect(screen.getByRole('button', {name: 'Confirm unlink'})).toBeInTheDocument()
+    })
+
+    it('should render the cancel button', () => {
+      render(getUnlinkingConfirmingStateComponent)
+      expect(screen.getByRole('button', {name: 'Cancel'})).toBeInTheDocument()
+    })
+
+    it('should render the primary MID confirmation message', () => {
+      render(getUnlinkingConfirmingStateComponent)
+      expect(screen.getByText('Are you sure you want to unlink this MID?')).toBeInTheDocument()
+    })
+
+    it('should render the Secondary MID confirmation message', () => {
+      render(getLocationMidsListItemComponent({
+        isSecondaryMid: true,
+        isInUnlinkingConfirmationState: true,
+      }))
+
+      expect(screen.getByText('Are you sure you want to unlink this Secondary MID?')).toBeInTheDocument()
+    })
+
+    it('should call the unlink function when the unlink confirmation button is clicked', () => {
+      render(getUnlinkingConfirmingStateComponent)
+      fireEvent.click(screen.getByRole('button', {name: 'Confirm unlink'}))
+
+      expect(mockProps.unlinkFn).toHaveBeenCalled()
+    })
+
+    it('should call the setUnlinkingMidFn function with null when the cancel button is clicked', () => {
+      render(getUnlinkingConfirmingStateComponent)
+      fireEvent.click(screen.getByRole('button', {name: 'Cancel'}))
+
+      expect(mockProps.setUnlinkingMidFn).toHaveBeenCalledWith(null)
+    })
   })
 })
