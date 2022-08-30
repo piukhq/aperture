9ffc9d6c42529909b21631b5811a8ef370ecb190
Lukie Kang <lkang@bink.com>
2022-08-30 11:41:12 +0100

    Readded the invalidTag for linkedMids

--- src/services/midManagementMerchantLocations.ts
+++ src/services/midManagementMerchantLocations.ts
@@ -130,6 +130,7 @@ export const midManagementMerchantLocationsApi = createApi({
           console.error('Error:', err)
         }
       },
+      invalidatesTags: ['MerchantLocationLinkedMids'],
     }),
     deleteMerchantLocationMidLink: builder.mutation<void, MerchantLocationsEndpointRefs>({
       query: ({planRef, merchantRef, midRef}) => ({
