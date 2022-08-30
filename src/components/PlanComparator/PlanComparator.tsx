65cb0780142d4410b6da7a720831354279f039bd
Lukie Kang <lkang@bink.com>
2022-08-30 11:06:59 +0100

    more experimenting

--- src/components/PlanComparator/PlanComparator.tsx
+++ src/components/PlanComparator/PlanComparator.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
-
 import {SelectedPlans} from 'types'
 import {capitaliseFirstLetter} from 'utils/stringFormat'
 import compare from 'just-compare'
+
 
 type Props = {
   plans: SelectedPlans
@@ -28,6 +28,7 @@ const PlanComparator = ({plans}: Props) => {
 
   const planCategories = [
     PlanCategory.ACCOUNT,
+    PlanCategory.BALANCES,
     PlanCategory.CARD,
     PlanCategory.CONTENT,
     PlanCategory.FEATURE_SET,
@@ -71,26 +72,62 @@ const PlanComparator = ({plans}: Props) => {
     }, 0)
 
     const doesCategoryKeyMatch = (categoryKey: string) => plansArray.every((plan) => compare(plan[category][categoryKey], plansArray[0][category][categoryKey]))
-    const categoryMatchingPercentage = (matchingCategoryKeyTotal / categoryKeys.length) * 100
-    const isEmptyCategory = categoryKeys.length === 0
+    const totalKeysInCategory = categoryKeys.length
+    const isEmptyCategory = totalKeysInCategory === 0
+    const isFullyMatchedCategory = matchingCategoryKeyTotal === totalKeysInCategory
 
 
-    const renderDetails = () => {
-      return `${matchingCategoryKeyTotal}/${categoryKeys.length} matches`
+    const renderCategorySummaryInformation = () => {
+      if (isEmptyCategory) {
+        return 'unused'
+      } else if (isFullyMatchedCategory) {
+        return 'fully matched'
+      } else {
+        return `${matchingCategoryKeyTotal} matches out of ${categoryKeys.length}`
+      }
     }
+
+
+    const renderCategoryDetails = () => {
+      const renderMismatchedCategoryKey = (categoryKey: string) => {
+        return (
+          <details className='m-5 bg-red/20 rounded-[10px] p-[10px]'>
+            <summary className='font-heading-8' key={categoryKey}>
+              {categoryKey}
+            </summary>
+          </details>
+        )
+      }
+
+
+      const renderCategoryHeader = () => {
+        if (isEmptyCategory) {
+          return <p className='font-body-3 italic my-5'> No environments use this category</p>
+        }
+        if (isFullyMatchedCategory) {
+          return <p className='font-body-3 italic my-5'> {capitaliseFirstLetter(category)} has {totalKeysInCategory} identical properties across all logged-in environments</p>
+        } else {
+          return <p className='font-heading-7 my-5'>Mismatched Keys</p>
+        }
+      }
+
+
+      return (
+        <>
+          {renderCategoryHeader()}
+          {!isFullyMatchedCategory && categoryKeys.map(categoryKey => !doesCategoryKeyMatch(categoryKey) && renderMismatchedCategoryKey(categoryKey))}
+        </>
+      )
+    }
+
+
     return (
-      <details className='bg-grey-200 m-[20px] border-4 border-grey-400 rounded-[10px] min-h-[50px] p-[10px]'>
-        <summary className={`flex space-between cursor-pointer font-heading-5 ${categoryMatchingPercentage < 75 && 'text-red'}`}>
-
-
-          <span>
-            {capitaliseFirstLetter(category)}
-          </span>
-          <span>
-            {isEmptyCategory ? 'Blank' : renderDetails()}
-          </span>
+      <details className={`m-[20px] rounded-[10px] min-h-[50px] p-[20px] shadow-md
+        ${isEmptyCategory ? 'bg-grey-200' : isFullyMatchedCategory ? 'bg-green/10' : 'bg-red/10'}`}>
+        <summary className='w-full cursor-pointer font-heading-5'>
+          {capitaliseFirstLetter(category)}{' '}<span className='italic font-body-3 ml-1'>({renderCategorySummaryInformation()})</span>
         </summary>
-        {categoryKeys.map(categoryKey => <p className={ doesCategoryKeyMatch(categoryKey) ? 'text-green' : 'text-red'} key={categoryKey}>{categoryKey}</p>)}
+        {renderCategoryDetails()}
       </details>
     )
   }
@@ -113,7 +150,6 @@ export default PlanComparator
 // 2. Missing category in one env
 // 3. Blank in all env
 // 4 only one env?
-// 4. Categories with sub categories
 // 5. Images, and content ordering
 
 
