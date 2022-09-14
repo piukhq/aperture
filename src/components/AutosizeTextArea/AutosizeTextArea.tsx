25994fc5319a6424201ae2b2184e514effe031bb
George <godonnell@bink.com>
2022-09-14 15:30:10 +0100

    Tweaked the aria label output

--- src/components/AutosizeTextArea/AutosizeTextArea.tsx
+++ src/components/AutosizeTextArea/AutosizeTextArea.tsx
@@ -21,7 +21,7 @@ const AutosizeTextArea = ({accessibilityLabel, placeholder, buttonClickHandler}:
   return (
     <div className='relative flex w-full'>
       <textarea
-        aria-label={accessibilityLabel}
+        aria-label={`${accessibilityLabel} text area`}
         data-testid='textarea'
         className={
           'scrollbar-hidden w-full min-h-[50px] max-h-[85px] resize-none pl-[12px] py-[11px] pr-[40px] rounded-[10px] border-[1px] border-grey-500 dark:bg-grey-825 font-body-3  focus:outline-lightBlue'
@@ -32,7 +32,7 @@ const AutosizeTextArea = ({accessibilityLabel, placeholder, buttonClickHandler}:
         rows={1}
         value={value}
       />
-      <button data-testid='submit-button' onClick={buttonClickHandler} aria-label={accessibilityLabel} className='absolute right-[10px] bottom-[14px]'>
+      <button data-testid='submit-button' onClick={buttonClickHandler} aria-label={`${accessibilityLabel} button`} className='absolute right-[10px] bottom-[14px]'>
         <ArrowRightSvg className='h-[22px] w-[22px]' />
       </button>
     </div>
