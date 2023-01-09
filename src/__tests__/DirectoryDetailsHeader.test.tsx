09e42eb163cfcc861bdfdbc363212cd64c1e9c65
Lukie Kang <lkang@bink.com>
2023-01-09 15:15:19 +0000

    bug: add missing images fallback and add relevant unit tests

--- src/__tests__/DirectoryDetailsHeader.test.tsx
+++ src/__tests__/DirectoryDetailsHeader.test.tsx
@@ -14,11 +14,8 @@ const mockPlanId = 1234
 const mockLocationLabel = 'mock_cafe'
 const mockOptionsMenuItems = [{label: 'mock_option_1', icon: jest.fn(), clickHandler: jest.fn()}]
 
-const getDirectoryDetailsHeaderPlanComponent = () => (
-  <DirectoryDetailsHeader name={mockName} iconUrl={mockIconUrl} slug={mockSlug} planId={mockPlanId} newItemButtonHandler={() => null} optionsMenuItems={mockOptionsMenuItems}/>
-)
-const getDirectoryDetailsHeaderMerchantComponent = () => (
-  <DirectoryDetailsHeader name={mockName} iconUrl={mockIconUrl} slug={mockSlug} planId={mockPlanId} locationLabel={mockLocationLabel} optionsMenuItems={mockOptionsMenuItems} isMerchant />
+const getDirectoryDetailsHeaderComponent = (passedProps = {}) => (
+  <DirectoryDetailsHeader name={mockName} iconUrl={mockIconUrl} slug={mockSlug} planId={mockPlanId} newItemButtonHandler={() => null} optionsMenuItems={mockOptionsMenuItems} {...passedProps}/>
 )
 
 describe('DirectoryDetailsHeader', () => {
@@ -34,7 +31,7 @@ describe('DirectoryDetailsHeader', () => {
   })
 
   it('should render correct metadata', () => {
-    render(getDirectoryDetailsHeaderPlanComponent())
+    render(getDirectoryDetailsHeaderComponent())
 
     expect(screen.getByText(mockName)).toBeInTheDocument()
     expect(screen.getByText(mockSlug)).toBeInTheDocument()
@@ -42,19 +39,26 @@ describe('DirectoryDetailsHeader', () => {
   })
 
   it('should render the icon image', () => {
-    render(getDirectoryDetailsHeaderPlanComponent())
+    render(getDirectoryDetailsHeaderComponent())
 
     expect(screen.queryByTestId('icon-image')).toBeInTheDocument()
   })
 
+  it('should not render the icon image', () => {
+    render(getDirectoryDetailsHeaderComponent({iconUrl: null}))
+
+    expect(screen.queryByTestId('icon-image')).not.toBeInTheDocument()
+    expect(screen.queryByTestId('icon-placeholder')).toBeInTheDocument()
+  })
+
   it('should render the OptionsMenuButton component', () => {
-    render(getDirectoryDetailsHeaderPlanComponent())
+    render(getDirectoryDetailsHeaderComponent())
     expect(screen.getByTestId('options-menu-button')).toBeInTheDocument()
   })
 
   describe('Plan-specific behavior', () => {
     it('should not render the location label', () => {
-      render(getDirectoryDetailsHeaderPlanComponent())
+      render(getDirectoryDetailsHeaderComponent())
       const locationLabel = screen.queryByText(mockLocationLabel)
       const locationLabelHeader = screen.queryByText('Location Label')
 
@@ -63,22 +67,23 @@ describe('DirectoryDetailsHeader', () => {
     })
 
     it('should render the New Merchant button', () => {
-      render(getDirectoryDetailsHeaderPlanComponent())
+      render(getDirectoryDetailsHeaderComponent())
       expect(screen.getByRole('button', {name: 'New Merchant'})).toBeInTheDocument()
     })
   })
 
   describe('Merchant-specific behavior', () => {
     it('should render the location label', () => {
-      render(getDirectoryDetailsHeaderMerchantComponent())
+      render(getDirectoryDetailsHeaderComponent({isMerchant: true, locationLabel: mockLocationLabel}))
       const locationLabel = screen.queryByText(mockLocationLabel)
       const locationLabelHeader = screen.queryByText('Location Label')
 
       expect(locationLabel).toBeInTheDocument()
       expect(locationLabelHeader).toBeInTheDocument()
     })
+
     it('should not render the New Merchant button', () => {
-      render(getDirectoryDetailsHeaderMerchantComponent())
+      render(getDirectoryDetailsHeaderComponent({isMerchant: true}))
       expect(screen.queryByRole('button', {name: 'New Merchant'})).not.toBeInTheDocument()
     })
   })
