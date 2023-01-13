506308b5ed12a9ea76ad726ba858f2f84c320c3b
Lukie Kang <lkang@bink.com>
2023-01-13 12:57:16 +0000

    test: simplify auth0 mock to essentials

--- jest.setup.js
+++ jest.setup.js
@@ -3,20 +3,10 @@ import '@testing-library/jest-dom/extend-expect'
 import 'whatwg-fetch'
 import {UserPermissions} from './src/utils/enums'
 
-jest.mock('@auth0/nextjs-auth0', () => ({
-  Auth0Provider: ({children}) => children,
-  withAuthenticationRequired: (component, _) => component,
+jest.mock('@auth0/nextjs-auth0', () => ({ // Mocks auth0 as an admin user
   withPageAuthRequired: (component) => component,
-  useAuth0: () => {
-    return {
-      isLoading: false,
-      user: {sub: 'testUser'},
-      isAuthenticated: true,
-      loginWithRedirect: jest.fn(),
-    }
-  },
   useUser: () => {
-    return {user: {name: 'Test User', permissions: [
+    return {user: {permissions: [
       UserPermissions.MERCHANT_DATA_READ_ONLY,
       UserPermissions.MERCHANT_DATA_READ_WRITE,
       UserPermissions.MERCHANT_DATA_READ_WRITE_DELETE,
