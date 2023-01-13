76f1b84c630c03a5091225290502372fd10f6050
Lukie Kang <lkang@bink.com>
2023-01-13 12:52:49 +0000

    test: add mock Auth0 as a placeholder to allow tests to run as an admin user

--- jest.setup.js
+++ jest.setup.js
@@ -1,3 +1,25 @@
 // jest.setup.js
 import '@testing-library/jest-dom/extend-expect'
 import 'whatwg-fetch'
+import {UserPermissions} from './src/utils/enums'
+
+jest.mock('@auth0/nextjs-auth0', () => ({
+  Auth0Provider: ({children}) => children,
+  withAuthenticationRequired: (component, _) => component,
+  withPageAuthRequired: (component) => component,
+  useAuth0: () => {
+    return {
+      isLoading: false,
+      user: {sub: 'testUser'},
+      isAuthenticated: true,
+      loginWithRedirect: jest.fn(),
+    }
+  },
+  useUser: () => {
+    return {user: {name: 'Test User', permissions: [
+      UserPermissions.MERCHANT_DATA_READ_ONLY,
+      UserPermissions.MERCHANT_DATA_READ_WRITE,
+      UserPermissions.MERCHANT_DATA_READ_WRITE_DELETE,
+    ]}}
+  },
+}))
