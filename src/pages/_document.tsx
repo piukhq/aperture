526bc82114faac4060d0f6bba3816e0c756e997d
Lukie Kang <lkang@bink.com>
2023-01-30 10:26:30 +0000

    style: style scrollbars to match light nad dark  themes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ const Document = () => <Html>
     <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='true'/>
     <link href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&family=Roboto:wght@400;500;700&display=swap' rel='stylesheet'/>
   </Head>
-  <body>
+  <body className='bg-grey-200 dark:bg-grey-950'>
     <Main />
     <NextScript />
   </body>
