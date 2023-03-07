c08b19197dadca17f10dd71247878abe88fb3f61
Lukie Kang <lkang@bink.com>
2023-03-07 13:42:27 +0000

    change: style the expand collaspe button

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -25,7 +25,6 @@ const Sidebar = ({isOpen, setIsOpen}: Props) => {
   const {user = {}} = useUser()
   const {permissions = []} = user as AuthUser
   const [sidebarWidthClass, setSidebarWidthClass] = useState('w-64')
-
   const isApiReflectorEnabled = useAppSelector(getUseApiReflector)
 
   const [selectedTool, setSelectedTool] = useState('')
@@ -61,16 +60,16 @@ const Sidebar = ({isOpen, setIsOpen}: Props) => {
   const renderSidebarToggle = () => (
     <button
       onClick={() => setIsOpen(!isOpen)}
-      className='flex items-center justify-center h-16 w-16 hover:scale-125 duration-200 ease-in-out'
+      className={`flex items-center justify-center h-8 w-8 ease-in-out border-grey-400 hover:scale-125 rounded-full border-2 p-2 bg-white/90 dark:bg-black/50 duration-200 ${isOpen ? 'translate-x-12 ml-[6px] translate-y-12 rotate-90' : 'translate-x-2 translate-y-4 -rotate-90'}`}
     >
-      <ArrowDownSvg fill='blue' className={`${isOpen ? 'rotate-90' : '-rotate-90'} scale-125`} />
+      <ArrowDownSvg fill='grey' className='scale-125 pt-[1px]' />
     </button>
   )
 
   // Render the collapsed sidebar
   if (!isOpen) {
     return (
-      <nav className='flex w-full space-between duration-200 fixed z-50 bg-gradient-to-b from-grey-200 to-grey-200/90 dark:from-grey-900 dark:to-900/90'>
+      <nav className='flex w-full space-between duration-200 fixed z-50 bg-gradient-to-b from-grey-200 via-grey-200 to-grey-200/5 dark:from-grey-900 dark:to-900/90'>
         <Link href='/' passHref>
           <div onMouseOver={() => setIsLogoHovered(true)} onMouseLeave={() => setIsLogoHovered(false)} className='flex h-16 pl-[25px] items-center ml-6 cursor-pointer'>
             <Image className={`${isLogoHovered && 'hue-rotate-[170deg]'} opacity-60 duration-[7s] skew-x-12`} data-testid='logo' src='/icons/svgs/aperture-logo-large.svg' height={35} width={35} alt='' />
