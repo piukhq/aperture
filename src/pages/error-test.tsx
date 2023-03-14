6c81cd4b7fca55d98f925867fc2566767d5196e7
Lukie Kang <lkang@bink.com>
2023-03-14 16:23:52 +0000

    change: add margin to Error Test page

--- src/pages/error-test.tsx
+++ src/pages/error-test.tsx
@@ -7,7 +7,7 @@ const ErrorTest = () => {
   }
 
   return (
-    <div className='w-full flex-col justify-center items-center gap-4 overflow-hidden'>
+    <div className='m-8 w-full flex-col justify-center items-center gap-4 overflow-hidden'>
       <div className={'flex flex-col gap-4 items-center'}>
         <h2 className='font-heading-4'>Error Test Page</h2>
         <p className='font-body-1 font-medium mt-4 mb-8'>This page allows us to test what happens when we encounter a client-side error</p>
