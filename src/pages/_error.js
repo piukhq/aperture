2eb9f489cc7982e1f0f6cd2db4e36a4c2e0fe2be
Lukie Kang <lkang@bink.com>
2023-03-15 12:06:16 +0000

    feat: update error pages with  actual fake errors

--- src/pages/_error.js
+++ src/pages/_error.js
@@ -1,7 +1,6 @@
-import {ButtonBackground, ButtonSize, ButtonWidth, LabelColour} from 'components/Button/styles'
-import {ApertureHeader, Button} from 'components'
+import Link from 'next/link'
+import {ApertureHeader} from 'components'
 import {Admin} from 'utils/enums'
-
 function Error ({statusCode}) {
   return (
     <div className='w-full flex-col justify-center items-center gap-4 overflow-hidden'>
@@ -9,14 +8,9 @@ function Error ({statusCode}) {
       <div className={'flex flex-col gap-4 items-center'}>
         <h2 className='font-heading-4'>Oops! Something went wrong</h2>
         <p className='font-body-1 font-medium mt-4 mb-8'>The action you have taken has failed {statusCode && 'on the server.'}. Please try again or let us know if this persists</p>
-        <Button
-          buttonSize={ButtonSize.MEDIUM_ICON}
-          buttonWidth={ButtonWidth.MEDIUM}
-          buttonBackground={ButtonBackground.ORANGE}
-          labelColour={LabelColour.WHITE}
-          handleClick={() => { window.location.href = '/' }}
-        >Go Home
-        </Button>
+        <Link href='/' passHref >
+          <p className='flex justify-center items-center mt-4 text-white text-md hover:bg-orange/75 bg-orange text-center rounded-lg font-heading-7 text-sm duration-500 w-[120px] h-[38px] pointer-cursor'>Go Home</p>
+        </Link>
         <a className='flex justify-center items-center mt-4 text-white text-md hover:bg-red/75 bg-red text-center rounded-lg font-heading-7 text-sm duration-500 w-[120px] h-[38px]' target='_blank' href={`https://teams.microsoft.com/l/chat/0/0?users=${Admin.EMAIL}&topicName=Aperture Feedback&message=Hey ${Admin.FIRST_NAME}, I was using Aperture when I received a 'Something went wrong' error when I was...`} rel='noreferrer'>
             Let Us Know
         </a>
--- src/pages/error-test.tsx
+++ src/pages/error-test.tsx
@@ -1,24 +1,57 @@
 import {ButtonBackground, ButtonSize, ButtonWidth, LabelColour} from 'components/Button/styles'
+import {getSelectedDirectoryPlan, setSelectedDirectoryPlan} from 'features/directoryPlanSlice'
+import {useAppSelector, useAppDispatch} from 'app/hooks'
 import {Button} from 'components'
+import {useState} from 'react'
 
 const ErrorTest = () => {
+  // The logic with Redux selectors replicates an issue seen in the wild as the only way I can think of to recreate it while still fooling EsLint and Typescript into letting me build it successfully
+  const [isGoingToCrash, setIsGoingToCrash] = useState(false)
+  const selectedPlan = useAppSelector(getSelectedDirectoryPlan)
+  const dispatch = useAppDispatch()
+
+  isGoingToCrash && dispatch(setSelectedDirectoryPlan({
+    plan_ref: 'test',
+    plan_metadata: {name: 'test', icon_url: ''},
+    plan_counts: {merchants: 1, locations: 1, payment_schemes: []},
+  }))
+
   const throwError = () => {
-    throw new Error('This is a test error because you clicked the button')
+    setIsGoingToCrash(true)
+    console.log(selectedPlan)
+  }
+
+  const throwException = () => {
+    throw new Error('This is a test exception because you clicked the Generate Exception button')
   }
 
   return (
     <div className='m-8 w-full flex-col justify-center items-center gap-4 overflow-hidden'>
       <div className={'flex flex-col gap-4 items-center'}>
-        <h2 className='font-heading-4'>Error Test Page</h2>
-        <p className='font-body-1 font-medium mt-4 mb-8'>This page allows us to test what happens when we encounter a client-side error</p>
+        <h2 className='font-heading-1'>Error Test Page 💥</h2>
+        <p className='font-body-1 font-medium mt-4 '>This page allows us to test what happens when we encounter errors in Aperture</p>
+        <p className='font-body-2 font-medium mt-16 mb-2 w-1/2 text-center'> This button causes a recoverable error that shows a message in the console as well as generating an event in Sentry.</p>
         <Button
           buttonSize={ButtonSize.MEDIUM_ICON}
-          buttonWidth={ButtonWidth.MEDIUM}
+          buttonWidth={ButtonWidth.AUTO}
           buttonBackground={ButtonBackground.ORANGE}
+          labelColour={LabelColour.WHITE}
+          handleClick={throwException}
+        >Generate Exception
+        </Button>
+        <p className='font-body-2 font-medium mt-16 mb-2 w-1/2 text-center'> This button will cause the app to crash completely and should render a custom error page when not in development mode.</p>
+        <Button
+          buttonSize={ButtonSize.MEDIUM_ICON}
+          buttonWidth={ButtonWidth.AUTO}
+          buttonBackground={ButtonBackground.RED}
           labelColour={LabelColour.WHITE}
           handleClick={throwError}
         >Generate Error
         </Button>
+        <p className='font-body-2 font-medium mt-16 mb-2 w-1/2 text-center'> This button will open a new tab to a page that does not exist and thus show the 404 page instead.</p>
+        <a className='flex justify-center items-center text-white text-md hover:bg-green/75 bg-green text-center rounded-lg font-heading-7 text-sm duration-500 w-[120px] h-[38px] pointer-cursor' target='_blank' href={'/thispagedoesnotexistsoitwillgivea404error'} rel='noreferrer'>
+          Generate 404 Page
+        </a>
 
       </div>
     </div>
