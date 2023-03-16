7499ab9c2e9134b56a3cfc4cc9be6c071a9806f7
Lukie Kang <lkang@bink.com>
2023-03-16 11:24:23 +0000

    test: update page not found unit tests

--- src/components/PageNotFound/PageNotFound.test.tsx
+++ src/components/PageNotFound/PageNotFound.test.tsx
@@ -7,4 +7,16 @@ describe('Page Not Found', () => {
     render(< PageNotFound />)
     expect(screen.getByRole('heading', {name: 'Oops! Page not found'})).toBeInTheDocument()
   })
+  it('should render the Page Not Found copy', () => {
+    render(< PageNotFound />)
+    expect(screen.getByText('Sorry, the page you\'re looking for doesn\'t exist')).toBeInTheDocument()
+  })
+  it('should render the Go Home button', () => {
+    render(< PageNotFound />)
+    expect(screen.getByText('Go Home')).toBeInTheDocument()
+  })
+  it('should render the Let Us Know button', () => {
+    render(< PageNotFound />)
+    expect(screen.getByRole('link', {name: 'Let Us Know'})).toBeInTheDocument()
+  })
 })
