4fe2a8f1b65256d546e12d6c60c98ec19ee83e0d
Lukie Kang <lkang@bink.com>
2023-04-25 11:22:52 +0100

    test: SingleviewCombobox unit test

---
+++ src/components/Modals/components/DirectorySingleViewModal/components/SingleViewCombobox/SingleViewCombobox.test.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import {render, screen} from '@testing-library/react'
+import SingleViewCombobox from './SingleViewCombobox'
+
+
+describe('Dropdown', () => {
+
+  const mockProps = {
+    selectedEntity: 'mock_selected_entity',
+    availableEntities: ['mock_available_entity_1', 'mock_available_entity_2'],
+    entityValueFn: jest.fn(),
+    entityPaymentSchemeSlugFn: jest.fn(),
+    onChangeFn: jest.fn(),
+    shouldRenderPaymentCardIcon: true,
+    entityLabel: 'mockEntityLabel',
+    isDisabled: false,
+  }
+
+
+  describe('Test component renders', () => { // Limited tests as it is mostly Headless UI code
+    it('should render an input with the correct placeholder', () => {
+      render(<SingleViewCombobox {...mockProps} />)
+
+      expect(screen.getByPlaceholderText('Select mockEntityLabel')).toBeInTheDocument()
+    })
+  })
+})
