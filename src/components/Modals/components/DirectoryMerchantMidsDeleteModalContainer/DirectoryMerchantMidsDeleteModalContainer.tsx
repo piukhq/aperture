c9886f2682016c805d4962bb4be35940309de163
Lukie Kang <lkang@bink.com>
2023-04-27 15:41:27 +0100

    refactor: remove redundant mids plans reset

--- src/components/Modals/components/DirectoryMerchantMidsDeleteModalContainer/DirectoryMerchantMidsDeleteModalContainer.tsx
+++ src/components/Modals/components/DirectoryMerchantMidsDeleteModalContainer/DirectoryMerchantMidsDeleteModalContainer.tsx
@@ -1,13 +1,11 @@
 import {DirectoryMerchantEntityDeleteModal} from 'components'
 import {useRouter} from 'next/router'
-import {useAppSelector, useAppDispatch} from 'app/hooks'
+import {useAppSelector} from 'app/hooks'
 import {useMidManagementMids} from 'hooks/useMidManagementMids'
 import {RTKQueryErrorResponse, DirectoryMerchantEntitySelectedItem} from 'types'
 import {getSelectedDirectoryEntityCheckedSelection} from 'features/directoryMerchantSlice'
-import {midManagementPlansApi} from 'services/midManagementPlans'
 
 const DirectoryMerchantMidsDeleteModalContainer = () => {
-  const dispatch = useAppDispatch()
   const router = useRouter()
   const {merchantId, planId} = router.query
   const directoryEntityCheckedSelection = useAppSelector(getSelectedDirectoryEntityCheckedSelection) as DirectoryMerchantEntitySelectedItem[]
@@ -29,7 +27,6 @@ const DirectoryMerchantMidsDeleteModalContainer = () => {
   const deleteMids = () => {
     const checkedEntityRefs = directoryEntityCheckedSelection.map(entity => entity.entityRef)
     deleteMerchantMid({planRef: planId as string, merchantRef: merchantId as string, midRefs: checkedEntityRefs})
-    dispatch(midManagementPlansApi.util.resetApiState()) // Update plan list as count has changed
   }
 
   return (
