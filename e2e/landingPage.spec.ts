56515942ecabcaec9a2cbbdc22ad1cd0e996e36f
Lukie Kang <lkang@bink.com>
2023-05-10 09:20:47 +0100

    build: setup base url config and initial auth settings

--- e2e/landingPage.spec.ts
+++ e2e/landingPage.spec.ts
@@ -1,18 +1,19 @@
 import {test, expect} from '@playwright/test'
 
 test('has title', async ({page}) => {
-  await page.goto('https://playwright.dev/')
+  await page.goto('/')
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/)
+  await expect(page).toHaveTitle(/Aperture/)
 })
 
-test('get started link', async ({page}) => {
-  await page.goto('https://playwright.dev/')
 
-  // Click the get started link.
-  await page.getByRole('link', {name: 'Get started'}).click()
+// test('get started link', async ({page}) => {
+//   await page.goto('https://playwright.dev/')
 
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/)
-})
+//   // Click the get started link.
+//   await page.getByRole('link', {name: 'Get started'}).click()
+
+//   // Expects the URL to contain intro.
+//   await expect(page).toHaveURL(/.*intro/)
+// })
