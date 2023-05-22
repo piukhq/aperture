d7954d25d88688316c74ceb56b94fc03daff5dfd
Lukie Kang <lkang@bink.com>
2023-05-22 16:23:28 +0100

    style: : linting style tweaks

--- src/utils/classNames.ts
+++ src/utils/classNames.ts
@@ -1,9 +1,9 @@
-export function classNames(...classes: (false | null | undefined | string)[]) {
+export function classNames (...classes: (false | null | undefined | string)[]) {
   return classes.filter(Boolean).join(' ')
 }
 
 //usage example
-// <button className={classNames('this is always applied', 
+// <button className={classNames('this is always applied',
 //         isTruthy && 'this only when the isTruthy is truthy',
 //         active ? 'active classes' : 'inactive classes')}>
 //  Text
