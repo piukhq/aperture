02a6978025160315b82a0f06b7d2d8a750197234
Lukie Kang <lkang@bink.com>
2023-06-21 14:44:09 +0100

    fix: link with embedded a tag bug

--- src/components/PageNotFound/PageNotFound.tsx
+++ src/components/PageNotFound/PageNotFound.tsx
@@ -21,7 +21,7 @@ const PageNotFound = () => {
         <h2 className='font-heading-4'>Oops! Page not found</h2>
         <p className='font-body-1 font-medium mt-4 mb-8'>Sorry, the page you&apos;re looking for doesn&apos;t exist</p>
         <Link href='/' passHref >
-          <a className={`${commonLinkClasses} hover:bg-orange/75 bg-orange`}>Go Home</a>
+          <p className={`${commonLinkClasses} hover:bg-orange/75 bg-orange`}>Go Home</p>
         </Link>
         <a className={`${commonLinkClasses} hover:bg-red/75 bg-red`} target='_blank' href={`https://teams.microsoft.com/l/chat/0/0?users=${Admin.EMAIL}&topicName=Aperture Feedback&message=Hey ${Admin.FIRST_NAME}, I was using Aperture when I received a 'Page Not Found' error when I was...`} rel='noreferrer'>
         Let Us Know
