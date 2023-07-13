a4b79ca030755d4a7be4982aad2214b28902c315
Lukie Kang <lkang@bink.com>
2023-07-13 16:15:15 +0100

    refactor: add null type to Directory tile

--- src/components/DirectoryTile/DirectoryTile.tsx
+++ src/components/DirectoryTile/DirectoryTile.tsx
@@ -9,7 +9,7 @@ import {getCountWithCorrectNoun} from 'utils/stringFormat'
 
 type DirectoryTileMetadata = {
   name: string,
-  icon_url: string,
+  icon_url: string | null,
   slug?:string,
   plan_id?: number | null,
   location_label?: string
