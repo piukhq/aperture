47bfbdb93ed3137b69dc8931e6e36a6993eae313
Lukie Kang <lkang@bink.com>
2023-09-04 14:52:02 +0100

    change: update loading sketon to match new directory tile

--- src/components/DirectoryTile/DirectoryTileSkeleton.tsx
+++ src/components/DirectoryTile/DirectoryTileSkeleton.tsx
@@ -3,10 +3,11 @@ const DirectoryTileSkeleton = () => {
   const isMobileViewport = useIsMobileViewportDimensions()
 
   return (
-    <div className={`relative h-[330px] rounded-[20px] bg-white dark:bg-grey-825 shadow-md ${!isMobileViewport ? 'w-[330px]' : 'w-[270px]'}`}>
+    <div className={`relative h-[350px] rounded-[20px] bg-white dark:bg-grey-825 shadow-md ${!isMobileViewport ? 'w-[330px]' : 'w-[270px]'}`}>
       <div className='flex flex-col items-center mt-[28px] blur-sm'>
         <div className='w-[93px] rounded-[30px]'></div>
         <div className='w-24 h-24 bg-grey-200 dark:bg-grey-700 rounded-[30px] '></div>
+        <p className='font-heading-5 opacity-50 w-[118px] h-[30px] justify-between text-center rounded-lg'>Loading</p>
         <p className='font-heading-5 opacity-50 w-[118px] h-[30px] justify-between text-center rounded-lg'>Loading</p>
         <p className='font-subheading-5'>0 Locations</p>
         <div className='flex mt-[15px] mb-[25px] w-[218px] h-[40px] justify-between rounded-md'>
