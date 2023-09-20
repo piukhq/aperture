d1391c94d67ef79610fe68b5480da763765bae0b
Lukie Kang <lkang@bink.com>
2023-09-20 16:21:02 +0100

    change: add offboarding and onboarding capability to harmonia status button

--- src/components/Modals/components/DirectorySingleViewModal/components/HarmoniaStatus/HarmoniaStatus.tsx
+++ src/components/Modals/components/DirectorySingleViewModal/components/HarmoniaStatus/HarmoniaStatus.tsx
@@ -2,11 +2,16 @@ import {Button} from 'components'
 import {ButtonType, ButtonWidth, ButtonSize, ButtonBackground, LabelColour, LabelWeight} from 'components/Button/styles'
 import {useEffect, useState} from 'react'
 import {DirectoryTxmStatusDisplayValue, UserPermissions} from 'utils/enums'
+import {useAppDispatch} from 'app/hooks'
+import {setHasHarmoniaStatusUpdate} from 'features/directoryMerchantSlice'
+
 
 type Props = {
   txmStatus: string
   offboardEntityFn: () => void,
   onboardEntityFn: () => void,
+  shouldRefresh: boolean,
+  setShouldRefresh: (shouldRefresh: boolean) => void,
   isOnboardingLoading: boolean,
   isOnboardingSuccess: boolean,
   isOffboardingLoading: boolean,
@@ -19,6 +24,8 @@ const HarmoniaStatus = ({
   txmStatus,
   offboardEntityFn,
   onboardEntityFn,
+  shouldRefresh,
+  setShouldRefresh,
   isOnboardingLoading,
   isOnboardingSuccess,
   isOffboardingLoading,
@@ -26,10 +33,22 @@ const HarmoniaStatus = ({
   isDisabled,
 }: Props) => {
 
+  const dispatch = useAppDispatch()
   const [harmoniaStatusButtonAction, setHarmoniaStatusButtonAction] = useState<string>('')
   const harmoniaStatus = DirectoryTxmStatusDisplayValue[txmStatus]
 
-  const isLoading = isOnboardingLoading || isOffboardingLoading || harmoniaStatus === DirectoryTxmStatusDisplayValue.onboarding || harmoniaStatus === DirectoryTxmStatusDisplayValue.offboarding
+  const isOffboarding = (isOffboardingLoading || isOffboardingSuccess) && harmoniaStatus === DirectoryTxmStatusDisplayValue.onboarded
+  const isOnboarding = isOnboardingLoading || isOnboardingSuccess && harmoniaStatus === DirectoryTxmStatusDisplayValue.not_onboarded || DirectoryTxmStatusDisplayValue.offboarded
+
+  const buttonLabel = () => {
+    if (isOffboarding && shouldRefresh) {
+      return 'Offboarding...'
+    } else if (isOnboarding && shouldRefresh) {
+      return 'Onboarding...'
+    } else {
+      return harmoniaStatusButtonAction
+    }
+  }
 
   useEffect(() => {
     if (harmoniaStatus === DirectoryTxmStatusDisplayValue.onboarded) {
@@ -39,24 +58,30 @@ const HarmoniaStatus = ({
     }
   }, [harmoniaStatus, isOffboardingLoading, isOffboardingSuccess, isOnboardingLoading, isOnboardingSuccess, txmStatus])
 
+  const handleClick = () => {
+    setShouldRefresh(true)
+    dispatch(setHasHarmoniaStatusUpdate(true)) // Instructs the Single View Modal component to call for a refresh of the entity and entities on close
+    harmoniaStatus === DirectoryTxmStatusDisplayValue.onboarded ? offboardEntityFn() : onboardEntityFn()
+  }
+
   return (
     <section className='h-[38px] flex justify-between mb-[34px] items-center'>
       <div>
         <h2 className='font-modal-heading'>HARMONIA STATUS</h2>
-        <p className='font-modal-data' data-testid='harmonia-status'>{harmoniaStatus}</p>
+        <p className={`font-modal-data ${shouldRefresh && 'animate-pulse'}`} data-testid='harmonia-status'>{shouldRefresh ? buttonLabel() : harmoniaStatus}</p>
       </div>
       <Button
-        handleClick={() => harmoniaStatus === DirectoryTxmStatusDisplayValue.onboarded ? offboardEntityFn() : onboardEntityFn()}
+        handleClick={handleClick}
         buttonType={ButtonType.SUBMIT}
         buttonSize={ButtonSize.MEDIUM}
         buttonWidth={ButtonWidth.MEDIUM}
         buttonBackground={ButtonBackground.LIGHT_GREY}
         labelColour={LabelColour.GREY}
         labelWeight={LabelWeight.SEMIBOLD}
-        isDisabled={isDisabled || isLoading}
+        isDisabled={isDisabled || shouldRefresh}
         requiredPermission={UserPermissions.MERCHANT_DATA_READ_WRITE}
         noShadow
-      >{isLoading ? 'Loading' : harmoniaStatusButtonAction}
+      >{buttonLabel()}
       </Button>
     </section>
   )
